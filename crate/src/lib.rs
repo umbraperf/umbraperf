3374f591a0be4d1e2df16f9ae929eff57e148bd6
Nolimimer <schottmichaelsw@gmail.com>
2021-07-06 11:28:53 +0200

    First dummy csv accumlator

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -1,33 +1,98 @@
+pub(crate) mod csv_reader;
+
+extern crate csv;
+
+use csv_reader::example;
 use js_sys::Uint8Array;
 use wasm_bindgen::prelude::*;
 use std::sync::Arc;
-use std::sync::{Mutex, MutexGuard};
+use std::sync::Mutex;
+use csv::ReaderBuilder;
+use std::str;
 
 struct State {
     pub counter: u64,
+    pub vec: Vec<u8>,
 }
 
 thread_local! {
     static STATE: Arc<Mutex<State>> = Arc::new(Mutex::new(State {
         counter: 0,
+        vec: Vec::new(),
     }));
 }
 
 
 #[wasm_bindgen(js_name = "printSomething")]
 pub fn print_something(something: &str) {
-    web_sys::console::log_1(&something.into());
+    example();
 }
 
 
 #[wasm_bindgen(js_name = "consumeChunk")]
 pub fn consume_chunk(chunk: &Uint8Array) {
-    let buffer = chunk.to_vec();
+    
+    let _buffer = chunk.to_vec();
+    let number:u8 = 10;
+    let mut linevec = Vec::new();
+    let mut iterator = _buffer.iter();
+
     STATE.with(|s| {
-        let mut sg = s.lock().unwrap();
-        sg.counter += 1;
-        web_sys::console::log_1(&format!("received chunk: {}", sg.counter).into());
-    });
+        let mut sg = s.lock().expect("State unlocked");
+        if !sg.vec.len().eq(&0) {
+            for v in sg.vec.iter() {
+                linevec.push(*v);
+            }
+        }
+    });  
+
+    loop {
+        match iterator.next() {
+            Some(i) => {
+                if i == &number {
+                    print_to_console(&format!("BINARY: {:?}", &linevec).into());
+                    let s = str::from_utf8(&linevec).expect("Invalid UTF-8 sequence.");
+                    print_to_console(&format!("STRING: {:?}", &s).into());
+                    let mut rdr = ReaderBuilder::new()
+                    .delimiter(b',')
+                    .has_headers(false)
+                    .from_reader(s.as_bytes());
+        
+                    for result in rdr.records() {
+                        let record = result.expect("CSV record");
+                        let number = &record[1].parse::<u64>().expect("u64");
+                        STATE.with(|s| {
+                            let mut sg = s.lock().expect("State unlocked");
+                            sg.counter += number;
+                            print_to_console(&format!("COUNTER: {:?}", sg.counter).into());
+                        }); 
+                    }
+        
+                    linevec = Vec::new(); 
+        
+                } else {
+                   &linevec.push(*i);
+                }
+            },
+            None => { 
+                STATE.with(|s| {
+                    let mut sg = s.lock().expect("State unlocked");
+                    sg.vec = Vec::new();
+                    for v in linevec.iter() {
+                        sg.vec.push(*v);
+                    }
+                }); 
+                print_to_console(&format!("Chunk ends and rest of line may be moved to next chunk").into());
+                break;
+             }
+        }
+    }
+}
+
+pub fn print_to_console(str: &JsValue) {
+    unsafe {
+        web_sys::console::log_1(str);
+    }
 }
 
 #[wasm_bindgen(start)]
--- src/dummy.tsx
+++ src/dummy.tsx
@@ -19,10 +19,12 @@ class Dummy extends React.Component<Props> {
         const fileSize = file.size;
         let remaining = fileSize;
         let offset = 0;
+        
         while (remaining > 0) {
             const readHere = Math.min(remaining, 4000);
             let chunk = file.slice(offset, offset + readHere);
             const data = await chunk.arrayBuffer();
+            console.log(data)
             profiler_core.consumeChunk(new Uint8Array(data));
             remaining -= readHere;
             offset += readHere;
@@ -39,6 +41,36 @@ class Dummy extends React.Component<Props> {
     }
 
 }
+
+/* class Dummy extends React.Component<Props> {
+    public async passToMichael(event: React.ChangeEvent<HTMLInputElement>) {
+        const files = event.target.files;
+        if (!files || files?.length == 0 || files![0] == null) return;
+        const file = files[0];
+        const fileSize = file.size;
+        let remaining = fileSize;
+        let offset = 0;
+        while (remaining > 0) {
+            const readHere = Math.min(remaining, 4000);
+            let chunk = file.slice(offset, offset + readHere);
+            const data = await chunk.arrayBuffer();
+            console.log(data)
+            profiler_core.consumeChunk(new Uint8Array(data));
+            remaining -= readHere;
+            offset += readHere;
+        }
+    }
+
+    public render() {
+        return <div>
+            {this.props.helloworld}<br />
+            <button onClick={() => this.props.setHelloWorld("bar")} >SetBar</button>
+            <button onClick={() => profiler_core.printSomething("bam")} >CallIntoWasm</button>
+            <input type="file" onChange={(args: any) => this.passToMichael(args)} />
+        </div>;
+    }
+
+} */
 
 const mapStateToProps = (state: model.AppState) => ({
     helloworld: state.helloworld,

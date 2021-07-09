9d115203edc23cf443913ad3376cff1d9d44baf1
Johann Schworm <schworm@in.tum.de>
2021-07-09 15:14:59 +0200

    style dropzone, introduce css modules

--- src/dummy.tsx
+++ src/dummy.tsx
@@ -1,7 +1,7 @@
 import * as model from './model';
 import React from 'react';
 import { connect } from 'react-redux';
-import Dropzone from 'react-dropzone'
+import Dropzone, { DropzoneState } from 'react-dropzone'
 
 import * as profiler_core from '../crate/pkg';
 
@@ -22,7 +22,7 @@ class Dummy extends React.Component<Props> {
     }
 
     public async passToMichael(files: Array<File>) {
-       // const files = event.target.files;
+        // const files = event.target.files;
         if (!files || files?.length == 0 || files![0] == null) return;
         const file = files[0];
         const fileSize = file.size;
@@ -41,9 +41,9 @@ class Dummy extends React.Component<Props> {
         }
     }
 
-    public receiveFileOnDrop(acceptedFiles: Array<File>):void {
+    public receiveFileOnDrop(acceptedFiles: Array<File>): void {
 
-        if(acceptedFiles && acceptedFiles.length != 0 && acceptedFiles[0] != null){
+        if (acceptedFiles && acceptedFiles.length != 0 && acceptedFiles[0] != null) {
             this.props.setFileName(acceptedFiles[0].name);
             this.passToMichael(acceptedFiles);
         }
@@ -51,9 +51,10 @@ class Dummy extends React.Component<Props> {
 
     }
 
+
     public render() {
         return <div>
-{/*             {this.props.helloworld}<br />
+            {/*             {this.props.helloworld}<br />
             <button onClick={() => this.props.setHelloWorld("bar")} >SetBar</button>
             <button onClick={() => profiler_core.printSomething("bam")} >CallIntoWasm</button>
             <input type="file" onChange={(args: any) => this.passToMichael(args)} /> */}
@@ -62,15 +63,34 @@ class Dummy extends React.Component<Props> {
                 <p>
                     Selected file: {this.props.fileName}
                 </p>
-                <Dropzone accept = {".csv"} onDrop={(acceptedFiles, rejectedFiles) => this.receiveFileOnDrop(acceptedFiles)}>
-                    {({ getRootProps, getInputProps }) => (
-                        <section>
-                            <div {...getRootProps()}>
-                                <input {...getInputProps()} />
-                                <p>Drag 'n' drop some files here, or click to select files</p>
-                            </div>
-                        </section>
-                    )}
+                <Dropzone
+                    accept={'.csv'}
+                    multiple={false}
+                    onDrop={(acceptedFiles) => this.receiveFileOnDrop(acceptedFiles)}>
+                    {({ getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject, acceptedFiles, fileRejections }: DropzoneState): any => {
+
+                        const dropzoneStyle = () => {
+                            if (isDragActive) {
+                                return "dropzoneActive";
+                            } else if (acceptedFiles.length !== 0) {
+                                return "dropzoneAccept";
+                            } else if (fileRejections.length !== 0) {
+                                return "dropzoneReject"
+                            } else {
+                                return "dropzoneBase"
+                            }
+                        };
+
+                        console.log(fileRejections);
+
+                        return (
+                            <section className={styles[dropzoneStyle()]}>
+                                <div {...getRootProps()} style={{ width: "100%", height: "100%" }}>
+                                    <input {...getInputProps()} />
+                                    <p>Drag 'n' drop some files here, or click to select files</p>
+                                </div>
+                            </section>)
+                    }}
                 </Dropzone>
             </div>
         </div>;
@@ -124,6 +144,10 @@ const mapDispatchToProps = (dispatch: model.Dispatch) => ({
             type: model.StateMutationType.SET_FILENAME,
             data: newFileName,
         }),
+});
+
+const dropzoneStyle = () => ({
+
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dummy);

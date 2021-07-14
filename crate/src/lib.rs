055fef89708c267233cbb118398ac53491b26d5a
Nolimimer <schottmichaelsw@gmail.com>
2021-07-14 13:57:15 +0200

    Process last line of chunk

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -44,13 +44,13 @@ pub fn set_expected_chunks(expected_chunks: i32) -> i32 {
 #[wasm_bindgen(js_name = "consumeChunk")]
 pub fn consume_chunk(chunk: &Uint8Array) {
     let _buffer: Vec<u8> = chunk.to_vec();
-    let number: u8 = 10;
+    let linebreak: u8 = 10;
 
     let mut linevec = Vec::new();
     let mut iterator = _buffer.iter();
 
     STATE.with(|s| {
-        let mut sg = s.lock().expect("State unlocked");
+        let sg = s.lock().expect("State unlocked");
         if !sg.vec.len().eq(&0) {
             for v in sg.vec.iter() {
                 linevec.push(*v);
@@ -58,29 +58,13 @@ pub fn consume_chunk(chunk: &Uint8Array) {
         }
     });  
 
+    // loop through buffer
     loop {
         match iterator.next() {
             Some(i) => {
-                if i == &number {
-                    print_to_console(&format!("BINARY: {:?}", &linevec).into());
-                    let s = str::from_utf8(&linevec).expect("Invalid UTF-8 sequence.");
-                    print_to_console(&format!("STRING: {:?}", &s).into());
-                    let mut rdr = ReaderBuilder::new()
-                    .delimiter(b',')
-                    .has_headers(false)
-                    .from_reader(s.as_bytes());
-        
-                    for result in rdr.records() {
-                        let record = result.expect("CSV record");
-                        let number = &record[1].parse::<i32>().expect("u64");
-                        unsafe {
-                            COUNTER = COUNTER + number;
-                        }
-                    }
-                    unsafe {
-                        print_to_console(&format!("COUNTER: {:?}", COUNTER).into());
-                    }
-
+                // if linebreak is found process vector else push it to vector for later use
+                if i == &linebreak {
+                    process_chunk(linevec);
                     linevec = Vec::new(); 
         
                 } else {
@@ -90,18 +74,22 @@ pub fn consume_chunk(chunk: &Uint8Array) {
             None => { 
                 STATE.with(|s| {
                     let mut sg = s.lock().expect("State unlocked");
-                    sg.vec = Vec::new();
-                    for v in linevec.iter() {
-                        sg.vec.push(*v);
-                    }
                     sg.processed_chunks += 1;
                     if sg.expected_chunks == sg.processed_chunks {
+                        if linevec.len() > 0 {
+                            process_chunk(linevec);
+                        }
                         rustfunc();
                         unsafe {
                             COUNTER = 0;
                         }
                         sg.processed_chunks = 0;
                         sg.vec = Vec::new();
+                    } else {
+                        sg.vec = Vec::new();
+                        for v in linevec.iter() {
+                            sg.vec.push(*v);
+                        }
                     }
                 }); 
                 print_to_console(&format!("Chunk ends and rest of line may be moved to next chunk").into());
@@ -109,6 +97,29 @@ pub fn consume_chunk(chunk: &Uint8Array) {
              }
         }
     }
+}
+
+pub fn process_chunk(vec: Vec<u8>) {
+
+    print_to_console(&format!("BINARY: {:?}", &vec).into());
+    let s = str::from_utf8(&vec).expect("Invalid UTF-8 sequence.");
+    print_to_console(&format!("STRING: {:?}", &s).into());
+    let mut rdr = ReaderBuilder::new()
+    .delimiter(b',')
+    .has_headers(false)
+    .from_reader(s.as_bytes());
+
+    for result in rdr.records() {
+        let record = result.expect("CSV record");
+        let number = &record[1].parse::<i32>().expect("u64");
+        unsafe {
+            COUNTER = COUNTER + number;
+        }
+    }
+    unsafe {
+        print_to_console(&format!("COUNTER: {:?}", COUNTER).into());
+    }
+
 }
 
 pub fn print_to_console(str: &JsValue) {

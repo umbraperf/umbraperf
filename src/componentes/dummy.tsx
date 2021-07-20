65c8576863e130490b5e6fbfeb39a2bfc4276948
Johann Schworm <schworm@in.tum.de>
2021-07-20 12:44:03 +0200

    remove hello world from dummy

--- src/componentes/dummy.tsx
+++ src/componentes/dummy.tsx
@@ -10,8 +10,6 @@ import styles from '../style/dummy.module.css';
 import { CircularProgress } from '@material-ui/core';
 
 interface Props {
-    helloworld: string;
-    setHelloWorld: (newGreeter: string) => void;
     fileName: string | undefined;
     setFileName: (newFileName: string) => void;
     resultLoading: boolean;
@@ -152,36 +150,6 @@ class Dummy extends React.Component<Props> {
 
 }
 
-/* class Dummy extends React.Component<Props> {
-    public async passToMichael(event: React.ChangeEvent<HTMLInputElement>) {
-        const files = event.target.files;
-        if (!files || files?.length == 0 || files![0] == null) return;
-        const file = files[0];
-        const fileSize = file.size;
-        let remaining = fileSize;
-        let offset = 0;
-        while (remaining > 0) {
-            const readHere = Math.min(remaining, 4000);
-            let chunk = file.slice(offset, offset + readHere);
-            const data = await chunk.arrayBuffer();
-            console.log(data)
-            profiler_core.consumeChunk(new Uint8Array(data));
-            remaining -= readHere;
-            offset += readHere;
-        }
-    }
-
-    public render() {
-        return <div>
-            {this.props.helloworld}<br />
-            <button onClick={() => this.props.setHelloWorld("bar")} >SetBar</button>
-            <button onClick={() => profiler_core.printSomething("bam")} >CallIntoWasm</button>
-            <input type="file" onChange={(args: any) => this.passToMichael(args)} />
-        </div>;
-    }
-
-} */
-
 const mapStateToProps = (state: model.AppState) => ({
     helloworld: state.helloworld,
     fileName: state.fileName,
@@ -191,11 +159,6 @@ const mapStateToProps = (state: model.AppState) => ({
 });
 
 const mapDispatchToProps = (dispatch: model.Dispatch) => ({
-    setHelloWorld: (newGreeter: string) =>
-        dispatch({
-            type: model.StateMutationType.SET_GREETER,
-            data: newGreeter,
-        }),
     setFileName: (newFileName: string) =>
         dispatch({
             type: model.StateMutationType.SET_FILENAME,

9bfec9b7f13b0bf9a1c84a9926904e78618baa07
Johann Schworm <schworm@in.tum.de>
2021-08-04 20:08:48 +0200

    code cleanup web file controller

--- src/controller/web_file_controller.ts
+++ src/controller/web_file_controller.ts
@@ -1,7 +1,7 @@
 import { StateMutationType } from "../model/state_mutation";
 import { Result, createResultObject } from "../model/core_result";
 import store from '../app';
-import { WorkerAPI} from "src/worker_api";
+import { WorkerAPI } from "src/worker_api";
 
 
 export interface FileInfo {
@@ -11,49 +11,16 @@ export interface FileInfo {
 
 
 export class WebFileController {
+
     worker: WorkerAPI;
 
     constructor(worker: WorkerAPI) {
         this.worker = worker;
     }
 
-
-    public getLength() {
-        if (undefined !== store.getState().file) {
-            return store.getState().file?.size as number;
-        }
-
-    }
-
     public registerFileAtWorker(file: File) {
         this.worker.registerFile(file);
-
-        //TODO remove:
-        //this.worker.readChunk(0, 100);
-        //let a = this.askJsForChunk(0, 10);
-
     }
-
-/*     public askJsForChunk(offset: number, chunkSize: number) {
-
-        const awaitPromise = async () => {
-            const resolvedPromise = (await this.worker.readChunk(offset, chunkSize)) as Uint8Array;
-            console.log(resolvedPromise);
-            return resolvedPromise;
-          };
-
-        return awaitPromise().then(
-            (result) => {
-                return result;
-            }
-        ).catch(
-            (error) => {
-                return error;
-            }
-        );
-    } */
-
-
 }
 
 export function storeResultFromRust(request: string, x: Array<any>, y?: Array<any>, z?: Array<any>) {

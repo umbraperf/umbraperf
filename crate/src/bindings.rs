b0c1c3ec621474046d22ab15fabb21c01e97ed98
Michael Schott <schottmichaelsw@gmail.com>
2021-08-05 22:43:45 +0200

    Store result in rust

--- crate/src/bindings.rs
+++ crate/src/bindings.rs
@@ -3,9 +3,13 @@ use js_sys::Uint8Array;
 
 // WASM_BINDGEN
 #[wasm_bindgen(raw_module = "../../src/worker")]
-extern "C" {
+extern "C" { 
 
     #[wasm_bindgen(js_name = "readFileChunk")]
     pub fn read_file_chunk(offset: i32, bytes: i32) -> Uint8Array;
+
+    #[wasm_bindgen(js_name = "stroreResultFromRust")]
+    pub fn store_result_from_rust(result: i32, request_id: i32);
+
 
 }

591b3869eb96db762fa92efb2152b3c3fdba7aec
Johann Schworm <schworm@in.tum.de>
2021-08-20 14:52:23 +0200

    remove d3 bar chart, place dummy bar chart in bar chart component

--- src/components/charts/bar_chart.tsx
+++ src/components/charts/bar_chart.tsx
@@ -1,89 +1,129 @@
 import * as model from '../../model';
 import React from 'react';
 import { connect } from 'react-redux';
-import Dropzone, { DropzoneState, FileRejection } from 'react-dropzone'
-import * as d3 from 'd3';
-import styles from '../style/upload.module.css';
-import { CircularProgress } from '@material-ui/core';
-import { Result } from 'src/model/core_result';
 import { IAppContext, withAppContext } from '../../app_context';
-import { WebFileController } from '../../controller/web_file_controller';
+import { Vega } from 'react-vega';
+import { Result } from 'src/model/core_result';
+import { VisualizationSpec } from "../../../node_modules/react-vega/src";
+import styles from '../../style/charts.module.css';
+import { Redirect } from 'react-router-dom';
+
 
 interface Props {
     appContext: IAppContext;
-    file: undefined | File;
-    fileName: string | undefined;
     resultLoading: boolean;
     result: Result | undefined;
 }
 
 class BarChart extends React.Component<Props> {
 
-    barChartRef: React.RefObject<HTMLDivElement>;
-
     constructor(props: Props) {
         super(props);
 
-        this.barChartRef = React.createRef<HTMLDivElement>();
-
+        this.createVisualizationSpec = this.createVisualizationSpec.bind(this);
     }
 
-    createVisualization() {
-
-        const test: Result = {
-            request: 100,
-            x: ["test3", "test4", "test5"],
-            y: [5, 4, 3],
-            test: 1,
+    createVisualizationData() {
+        const data = {
+            name: 'table',
+            values: [
+                { category: 'A', amount: 28 },
+                { category: 'B', amount: 55 },
+                { category: 'C', amount: 43 },
+                { category: 'D', amount: 91 },
+                { category: 'E', amount: 81 },
+                { category: 'F', amount: 53 },
+                { category: 'G', amount: 19 },
+                { category: 'H', amount: 87 },
+            ],
         };
 
-    
-        // set the dimensions and margins of the graph
-        const margin = { top: 30, right: 30, bottom: 70, left: 60 },
-            width = 460 - margin.left - margin.right,
-            height = 400 - margin.top - margin.bottom;
+        return data;
+    }
 
-        // append the svg object to the body of the page
-        let svg = d3.select(this.barChartRef.current)
-            .append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform",
-                "translate(" + margin.left + "," + margin.top + ")");
+    createVisualizationSpec() {
+        const visData = this.createVisualizationData();
 
-        // Add X axis
-        const x = d3.scaleBand()
-            .range([0, width])
-            .domain(test.x!.map(function (d) { return d; }))
-            .padding(0.2);
-        svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x))
-            .selectAll("text")
-            .attr("transform", "translate(-10,0)rotate(-45)")
-            .style("text-anchor", "end");
+        const spec: VisualizationSpec = {
+            $schema: 'https://vega.github.io/schema/vega/v5.json',
+            width: 1000,
+            height: 500,
+            padding: { left: 5, right: 5, top: 5, bottom: 5 },
 
-        // Add Y axis
-        const y = d3.scaleLinear()
-            .domain([0, d3.max(test.x!) as number]).nice()
-            .range([height, 0]);
-        svg.append("g")
-            .call(d3.axisLeft(y));
+            data: [
+                visData
+            ],
 
-        // Bars
-        svg.selectAll("bar")
-            .data(test.x as Array<string>)
-            .enter()
-            .append("rect")
-            //.attr("x", (d) => x(d.id) || 0)
-            .attr("x", (d) => x(d) || 0)
-            //.attr("y", (d) => y(d.value))
-            .attr("y", (d, i) => y(test.x![i]))
-            .attr("width", x.bandwidth())
-            .attr("height", function (d, i) { return y(0) - y(test.y![i]); })
-            .attr("fill", "#69b3a2")
+            signals: [
+                {
+                    name: 'tooltip',
+                    value: {},
+                    on: [
+                        { events: 'rect:mouseover', update: 'datum' },
+                        { events: 'rect:mouseout', update: '{}' },
+                    ],
+                },
+            ],
 
+            scales: [
+                {
+                    name: 'xscale',
+                    type: 'band',
+                    domain: { data: 'table', field: 'category' },
+                    range: 'width',
+                },
+                {
+                    name: 'yscale',
+                    domain: { data: 'table', field: 'amount' },
+                    nice: true,
+                    range: 'height',
+                },
+            ],
+
+            axes: [
+                { orient: 'bottom', scale: 'xscale' },
+                { orient: 'left', scale: 'yscale' },
+            ],
+
+            marks: [
+                {
+                    type: 'rect',
+                    from: { data: 'table' },
+                    encode: {
+                        enter: {
+                            x: { scale: 'xscale', field: 'category', offset: 1 },
+                            width: { scale: 'xscale', band: 1, offset: -1 },
+                            y: { scale: 'yscale', field: 'amount' },
+                            y2: { scale: 'yscale', value: 0 },
+                        },
+                        update: {
+                            fill: { value: 'steelblue' },
+                        },
+                        hover: {
+                            fill: { value: 'red' },
+                        },
+                    },
+                },
+                {
+                    type: 'text',
+                    encode: {
+                        enter: {
+                            align: { value: 'center' },
+                            baseline: { value: 'bottom' },
+                            fill: { value: '#333' },
+                        },
+                        update: {
+                            x: { scale: 'xscale', signal: 'tooltip.category', band: 0.5 },
+                            y: { scale: 'yscale', signal: 'tooltip.amount', offset: -2 },
+                            text: { signal: 'tooltip.amount' },
+                            fillOpacity: [{ test: 'datum === tooltip', value: 0 }, { value: 1 }],
+                        },
+                    },
+                },
+            ],
+        } as VisualizationSpec;
+
+        return spec;
     }
 
     componentDidUpdate(prevProps: Props): void {
@@ -93,25 +133,25 @@ class BarChart extends React.Component<Props> {
     }
 
     componentDidMount(): void {
-        this.createVisualization();
+        //this.createVisualization();
     }
 
-    public render() {
-        return <div>
+    //const signalListeners = { hover: handleHover };
 
-            <div className={"resultArea"} >
-                {(this.props.resultLoading || this.props.result) ?
-                    (this.props.resultLoading ?
-                        <CircularProgress />
-                        :
-                        <div>
-                            <p>Result of computation from rust is: {this.props.result?.test}</p>
-                            <div id="bar-chart-ref" ref={this.barChartRef}></div>
-                        </div>
-                    )
-                    :
-                    <p>Select a file.</p>
-                }
+
+    public render() {
+        if (!this.props.result) {
+            return <Redirect to={"/upload"} />
+        }
+
+        return <div>
+            <div className={styles.resultArea} >
+                <div className={"vegaContainer"}>
+                    <Vega spec={this.createVisualizationSpec()} />
+                </div>
+            </div>
+            <div>
+                <p>Result of computation from rust is: {this.props.result?.test}</p>
             </div>
         </div>;
     }
@@ -119,11 +159,12 @@ class BarChart extends React.Component<Props> {
 }
 
 const mapStateToProps = (state: model.AppState) => ({
-    file: state.file,
-    fileName: state.fileName,
     resultLoading: state.resultLoading,
     result: state.result,
 });
 
 
 export default connect(mapStateToProps)(withAppContext(BarChart));
+
+
+

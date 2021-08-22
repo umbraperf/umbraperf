9ffcae29655843057ac591a5a5d76f8aea0ab6a2
Johann Schworm <schworm@in.tum.de>
2021-08-22 15:39:24 +0200

    remove apache arrow-js typescript, add apache-arrow js, add webpack loader for mjs, add webpack extension for mjs, fix apache arrow table import

--- src/worker_api.ts
+++ src/worker_api.ts
@@ -1,6 +1,7 @@
 import { storeResultFromRust } from './controller/web_file_controller';
 import * as model from './worker';
 //import {Table} from "@apache-arrow/ts";
+const table = require("../node_modules/apache-arrow/table");
 
 
 const worker = new Worker(new URL('./worker.ts', import.meta.url));
@@ -48,7 +49,7 @@ worker.addEventListener('message', message => {
         case model.WorkerResponseType.STORE_RESULT:
             console.log(messageData);
             //TODO:
-            //const arrowArray = Table.from(messageData);
+            const arrowArray = table.from(messageData);
             //console.log(arrowArray);
             console.log("main got result from worker.");
             storeResultFromRust(message.data.requestId, messageData);

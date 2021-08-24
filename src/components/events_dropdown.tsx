256605ac9d1726902b9745c52d85912764c3cb5e
Johann Schworm <schworm@in.tum.de>
2021-08-24 16:44:28 +0200

    trigger calculation of other event from dropdown menu

--- src/components/events_dropdown.tsx
+++ src/components/events_dropdown.tsx
@@ -64,7 +64,7 @@ export default function EventDropdownMenu() {
     const handleOnItemClick = (elem: string) => {
         handleClose();
         console.log(elem);
-        context?.controller.calculateChartData("bar_chart", elem);
+        context!.controller.calculateChartData("bar_chart", elem);
 
     };
 

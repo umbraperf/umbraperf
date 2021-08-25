c6bc84e5a9e15914e95af6a61fcd0974e226e9be
Johann Schworm <schworm@in.tum.de>
2021-08-25 15:14:13 +0200

    stacked data from arrays swim lanes

--- src/components/charts/swim_lanes.tsx
+++ src/components/charts/swim_lanes.tsx
@@ -29,39 +29,23 @@ interface State {
     height: number,
 }
 
+const testBuckets = [1,2,3,4,5];
+const testOperatorsGruop = [0, 0, 0.5, 0.7, 1];
+const testOperatorsJoin= [0, 0.5, 0.3, 0.3, 0];
+const testOperatorsTable = [1, 0.5, 0.2, 0, 0];
+
 const result = [[
-    { "x": 0, "y": 28, "c": 0 }, { "x": 0, "y": 55, "c": 1 },
-    { "x": 1, "y": 43, "c": 0 }, { "x": 1, "y": 91, "c": 1 },
-    { "x": 2, "y": 81, "c": 0 }, { "x": 2, "y": 53, "c": 1 },
-    { "x": 3, "y": 19, "c": 0 }, { "x": 3, "y": 87, "c": 1 },
-    { "x": 4, "y": 52, "c": 0 }, { "x": 4, "y": 48, "c": 1 },
-    { "x": 5, "y": 24, "c": 0 }, { "x": 5, "y": 49, "c": 1 },
-    { "x": 6, "y": 87, "c": 0 }, { "x": 6, "y": 66, "c": 1 },
-    { "x": 7, "y": 17, "c": 0 }, { "x": 7, "y": 27, "c": 1 },
-    { "x": 8, "y": 68, "c": 0 }, { "x": 8, "y": 16, "c": 1 },
-    { "x": 9, "y": 49, "c": 0 }, { "x": 9, "y": 15, "c": 1 }
+    { x: testBuckets, y: testOperatorsGruop, "c": 0 }, 
+    { x: testBuckets, y: testOperatorsJoin, "c": 1 }, 
+    { x: testBuckets, y: testOperatorsTable, "c": 2 }, 
 ], [
-    { "x": 0, "y": 28, "c": 3 }, { "x": 0, "y": 55, "c": 4 },
-    { "x": 1, "y": 43, "c": 3 }, { "x": 1, "y": 91, "c": 4 },
-    { "x": 2, "y": 81, "c": 3 }, { "x": 2, "y": 53, "c": 4 },
-    { "x": 3, "y": 19, "c": 3 }, { "x": 3, "y": 87, "c": 4 },
-    { "x": 4, "y": 52, "c": 3 }, { "x": 4, "y": 48, "c": 4 },
-    { "x": 5, "y": 24, "c": 3 }, { "x": 5, "y": 49, "c": 4 },
-    { "x": 6, "y": 87, "c": 3 }, { "x": 6, "y": 66, "c": 4 },
-    { "x": 7, "y": 17, "c": 3 }, { "x": 7, "y": 27, "c": 4 },
-    { "x": 8, "y": 68, "c": 3 }, { "x": 8, "y": 16, "c": 4 },
-    { "x": 100, "y": 49, "c": 3 }, { "x": 9, "y": 15, "c": 4 }
+    { x: testBuckets, y: testOperatorsGruop, "c": 0 }, 
+    { x: testBuckets, y: testOperatorsJoin, "c": 1 }, 
+    { x: testBuckets, y: testOperatorsTable, "c": 2 }, 
 ], [
-    { "x": 0, "y": 28, "c": 5 }, { "x": 0, "y": 55, "c": 6 },
-    { "x": 1, "y": 43, "c": 5 }, { "x": 1, "y": 91, "c": 6 },
-    { "x": 2, "y": 81, "c": 5 }, { "x": 2, "y": 53, "c": 6 },
-    { "x": 3, "y": 19, "c": 5 }, { "x": 3, "y": 87, "c": 6 },
-    { "x": 4, "y": 52, "c": 5 }, { "x": 4, "y": 48, "c": 6 },
-    { "x": 5, "y": 24, "c": 5 }, { "x": 5, "y": 49, "c": 6 },
-    { "x": 6, "y": 87, "c": 5 }, { "x": 6, "y": 66, "c": 6 },
-    { "x": 7, "y": 17, "c": 5 }, { "x": 7, "y": 27, "c": 6 },
-    { "x": 8, "y": 68, "c": 5 }, { "x": 8, "y": 16, "c": 6 },
-    { "x": 9, "y": 49, "c": 5 }, { "x": 9, "y": 15, "c": 6 }
+    { x: testBuckets, y: testOperatorsGruop, "c": 0 }, 
+    { x: testBuckets, y: testOperatorsJoin, "c": 1 }, 
+    { x: testBuckets, y: testOperatorsTable, "c": 2 }, 
 ]];
 
 const resultorig = [[
@@ -182,6 +166,10 @@ class SwimLanes extends React.Component<Props, State> {
             "name": "table",
             "values": result[chartId],
             "transform": [
+                { 
+                    type: "flatten", 
+                    fields: ["x", "y"] 
+                },
                 {
                     "type": "stack",
                     "groupby": ["x"],
@@ -271,7 +259,7 @@ class SwimLanes extends React.Component<Props, State> {
                             encode: {
                                 enter: {
                                     interpolate: {
-                                        value: "monotone"
+                                        value: "step"
                                     },
                                     x: {
                                         scale: "x",

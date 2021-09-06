b8351b48871a1f60c05782d083f5943dd7afbbb9
Johann Schworm <schworm@in.tum.de>
2021-09-06 21:52:01 +0200

    add hardcoded sql query to test group by parsing in rust

--- src/model/sql_queries.ts
+++ src/model/sql_queries.ts
@@ -1,7 +1,12 @@
+
+
+
 export enum SqlQueries{
 
     get_operators = "select operators from xy",
     get_events = 'select distinct ev_name from yx',
+    get_operator_frequency_per_event = "select operator, count(operator) from xy group by operator where ev_name = eventVonJs",
+    test = "select operator, count(operator) from xy group by operator where ev_name = eventVonJs",
     other = "other"
 
 }

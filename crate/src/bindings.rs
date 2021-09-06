ff798976875c49e958bb4119c0f581c558f05a74
Michael Schott <schottmichaelsw@gmail.com>
2021-09-06 18:10:52 +0200

    Add selection api first draft

--- crate/src/bindings.rs
+++ crate/src/bindings.rs
@@ -8,8 +8,8 @@ extern "C" {
     #[wasm_bindgen(js_name = "readFileChunk")]
     pub fn read_file_chunk(offset: i32, bytes: i32) -> Uint8Array;
 
-    #[wasm_bindgen(js_name = "storeEventsFromRust")]
-    pub fn send_events_to_js(events_results: Vec<u8>);
+    #[wasm_bindgen(js_name = "notifyJsFinishedReading")]
+    pub fn notify_js_finished_reading(request_id: i32);
 
     #[wasm_bindgen(js_name = "stroreArrowResultFromRust")]
     pub fn send_arrow_result_to_js(arrow_result: Vec<u8>);

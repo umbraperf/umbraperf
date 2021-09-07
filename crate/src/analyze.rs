d15cbe1116b827f61a8369627d5e4fd7bc3011a5
Michael Schott <ga48xop@in.tum.de>
2021-09-07 13:13:54 +0200

    Projection after group by change

--- crate/src/analyze.rs
+++ crate/src/analyze.rs
@@ -1,4 +1,4 @@
-use arrow::{array::{Array, ArrayRef, BooleanArray, Float64Array, Int64Array, PrimitiveArray, StringArray}, compute::{sort, sort_to_indices, sum, take}, datatypes::{Float64Type, Schema}, record_batch::RecordBatch};
+use arrow::{array::{Array, ArrayRef, BooleanArray, Float64Array, Int64Array, PrimitiveArray, StringArray}, compute::{sort, sort_to_indices, sum, take}, datatypes::{DataType, Field, Float64Type, Schema}, record_batch::RecordBatch};
 use log::Record;
 use std::{collections::{BTreeMap}, sync::Arc};
 use arrow::error::Result as ArrowResult;
@@ -156,25 +156,41 @@ use crate::{print_to_js, print_to_js_with_obj};
 
     }
 
-    pub fn count_rows_over(batch: &RecordBatch, column_to_groupby_over: usize, column_to_sum_over: usize) {
+    pub fn count_rows_over(batch: &RecordBatch, column_to_groupby_over: usize, column_to_sum_over: usize) -> RecordBatch {
 
         let unique_batch = find_unique_string(batch, column_to_groupby_over);
 
+        // Vector of unique strings
         let vec = unique_batch
         .column(0)
         .as_any()
         .downcast_ref::<StringArray>()
         .unwrap();
     
-        let mut builder =  Float64Array::builder(vec.len());  
+        // For each unique string there will be one result, therefore vec.len()
+        let mut result_builder =  Float64Array::builder(vec.len());  
     
         for group in vec {
+            // Filter unique string as filter_str
             let group_batch = filter_with(column_to_groupby_over, group.unwrap(), batch);
     
             let row_count = group_batch.num_rows() as f64;
     
-             builder.append_value(row_count);
+             result_builder.append_value(row_count);
         } 
+
+        let builder = result_builder.finish();
+
+        // old_schema + new count field
+        let field = Field::new("operator", DataType::Utf8, false);        
+        let result_field = Field::new("count", DataType::Float64, false);
+
+        let schema = Schema::new(vec![field, result_field]);
+
+        let vec = unique_batch.column(0).to_owned();
+
+        RecordBatch::try_new(Arc::new(schema), vec![vec, Arc::new(builder)]).unwrap()
+
     }
 
     pub fn sum_rows_over(batch: &RecordBatch, column_to_groupby_over: usize, column_to_sum_over: usize) {

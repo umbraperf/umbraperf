04b27c1d601c56252ff5e7ac34688565f2981420
Johann Schworm <schworm@in.tum.de>
2021-09-08 12:58:48 +0200

    add standard error for wrong sql queries from frontend, add dummy sql with event and bucket size for swim lanes to sql file

--- src/model/sql_queries.ts
+++ src/model/sql_queries.ts
@@ -7,12 +7,16 @@ export type SqlQuery<T, P> = {
 export enum SqlQueryType {
     GET_EVENTS = "GET_EVENTS",
     GET_OPERATOR_FREQUENCY_PER_EVENT = "GET_OPERATOR_FREQUENCY_PER_EVENT",
+    GET_REL_OP_DISTR_PER_BUCKET = "GET_REL_OP_DISTR_PER_BUCKET",
     other = "other"
 }
 
 export type QueryVariant =
     | SqlQuery<SqlQueryType.GET_EVENTS, {}>
-    | SqlQuery<SqlQueryType.GET_OPERATOR_FREQUENCY_PER_EVENT, { event: string }>;
+    | SqlQuery<SqlQueryType.GET_OPERATOR_FREQUENCY_PER_EVENT, { event: string }>
+    | SqlQuery<SqlQueryType.GET_REL_OP_DISTR_PER_BUCKET, { event: string, bucketSize: number }>
+    | SqlQuery<SqlQueryType.other, {}>
+    ;
 
 
 export function createSqlQuery(query: QueryVariant) {
@@ -22,6 +26,10 @@ export function createSqlQuery(query: QueryVariant) {
             return 'select distinct ev_name from yx';
         case SqlQueryType.GET_OPERATOR_FREQUENCY_PER_EVENT:
             return `select operator, count(operator)  from xy where ev_name = '${query.data.event}' group by operator`;
+        case SqlQueryType.GET_REL_OP_DISTR_PER_BUCKET:
+            return `dummy sql with ev_name = '${query.data.event}' and range = '${query.data.bucketSize}'`;
+        case SqlQueryType.other:
+            return 'error - bad request to backend';
     }
 }
 

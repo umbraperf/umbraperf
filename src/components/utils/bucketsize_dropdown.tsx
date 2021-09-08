49acc4d067556b294e397d6070ef7aa9bf7819e1
Johann Schworm <schworm@in.tum.de>
2021-09-08 13:10:42 +0200

    add bucket size to swim lane state, add bucket size dropdown props to swim lanes, add bucket size change handler to swim lanes,
    add bucketsize_dropdown file

---
+++ src/components/utils/bucketsize_dropdown.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import MenuItem from '@material-ui/core/MenuItem';
+import { InputLabel, Select } from '@material-ui/core';
+import styles from "../../style/dropdown.module.css";
+
+
+export default function BucketsizeDropdwn(props: any) {
+
+    const interpolations = ["linear", "linear-closed", "step", "step-before", "step-after", "basis", "basis-open", "basis-closed", "cardinal", "cardinal-open", "cardinal-closed", "bundle", "monotone"];
+
+    const handleOnItemClick = (elem: string) => {
+        props.changeInterpolation(elem);
+    };
+
+
+    return (
+        <div className={styles.interpolationDropdownSelectorContainer}>
+            <InputLabel className={styles.interpolationDropdownSelectorLabel} id="interpolation-selector-label">Choose Interpolation:</InputLabel>
+            <Select className={styles.interpolationDropdownSelector}
+                labelId="interpolation-selector-label"
+                id="interpolation-selector"
+                value={props.currentInterpolation}
+            >
+                {interpolations.map((elem, index) =>
+                    (<MenuItem onClick={() => handleOnItemClick(elem)} key={index} value={elem}>{elem}</MenuItem>)
+                )}
+            </Select>
+
+        </div>
+    );
+}

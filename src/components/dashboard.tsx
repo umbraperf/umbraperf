4b4b35192cd7a7090fe27e4ada595eca6d73b26b
Johann Schworm <schworm@in.tum.de>
2021-09-09 23:39:44 +0200

    dashboard first steps logic and styling

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -1,10 +1,12 @@
 import * as model from '../model';
 import React from "react";
-import { WidthProvider, Responsive } from "react-grid-layout";
 import _ from "lodash";
 import GridLayout from 'react-grid-layout';
 import { IAppContext, withAppContext } from '../app_context';
 import styles from '../style/dashboard.module.css';
+import { WidthProvider, Responsive } from 'react-grid-layout';
+
+
 
 
 interface Props {
@@ -13,10 +15,43 @@ interface Props {
 }
 
 interface State {
-  
+    items: Array<string>;
+    layout: {lg: Array<ILayoutElement>};
+    gridData: { w: number, h: number, x: number, y: number };
+    breakpoints: { lg: number, md: number, sm: number, xs: number, xxs: number };
+    cols: { lg: number, md: number, sm: number, xs: number, xxs: number };
+    rowHeight: number;
 
 }
 
+interface ILayoutElement {
+    i: string,
+    x: number,
+    y: number,
+    w: number,
+    h: number,
+    static?: boolean,
+    minW?: number,
+    maxW?: number,
+    minh?: number,
+    maxH?: number,
+}
+
+const originalItems: Array<string> = ["a", "b", "c", "d"];
+
+const initialLayouts = {
+    lg: [
+      { i: "a", x: 0, y: 0, w: 1, h: 4 },
+      { i: "b", x: 1, y: 0, w: 3, h: 4 },
+      { i: "c", x: 4, y: 0, w: 1, h: 4 },
+      { i: "d", x: 0, y: 4, w: 2, h: 4 }
+    ]
+  };
+
+const gridData = { w: 3, h: 2, x: 0, y: Infinity };
+
+//a HOC WidthProvider can be used to automatically determine width upon initialization and window resize events:
+const ResponsiveGridLayout = WidthProvider(Responsive);
 
 class Dashboard extends React.Component<Props, State> {
 
@@ -27,6 +62,12 @@ class Dashboard extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props);
         this.state = {
+            items: originalItems,
+            layout: initialLayouts,
+            gridData: gridData,
+            breakpoints: { lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 },
+            cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
+            rowHeight: 60,
 
         };
 
@@ -40,24 +81,32 @@ class Dashboard extends React.Component<Props, State> {
 
     }
 
-    createLayout(){
-      const layout = [
-        {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
-        {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
-        {i: 'c', x: 4, y: 0, w: 1, h: 2}
-      ];
-      return layout;
-    }
-
     render() {
-      return (
-        <GridLayout className="layout" layout={this.createLayout()} cols={12} rowHeight={30} width={1200}>
-          <div key="a">a</div>
-          <div key="b">b</div>
-          <div key="c">c</div>
-        </GridLayout>
-      );
-      }
+        return (
+            <ResponsiveGridLayout
+            className={`layout ${styles.gridBoxesContainer}`}
+            layouts={this.state.layout}
+                breakpoints={this.state.breakpoints}
+                cols={this.state.cols}
+                rowHeight={this.state.rowHeight}
+/*             onLayoutChange={onLayoutChange}
+ */          >
+                {this.state.items.map((key) => (
+                    <div
+                        key={key}
+                        className={`widget ${styles.gridBox}`}
+/*                         data-grid={this.state.gridData}
+ */                    >
+{/*                         <Widget
+                            id={key}
+/*                   onRemoveItem={onRemoveItem}
+                   backgroundColor="#867ae9"
+                        /> */}
+                    </div>
+                ))}
+            </ResponsiveGridLayout>
+        );
+    }
 
 }
 

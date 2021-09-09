fa1b90110284520432471940ac46f44384fa7dd9
Johann Schworm <schworm@in.tum.de>
2021-09-09 21:45:35 +0200

    working dummy dashboard with 3 letters

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -1,27 +1,37 @@
 import * as model from '../model';
 import React from "react";
-import { connect } from 'react-redux';
-import {withAppContext } from '../app_context';
+import { WidthProvider, Responsive } from "react-grid-layout";
+import _ from "lodash";
+import GridLayout from 'react-grid-layout';
+import { IAppContext, withAppContext } from '../app_context';
 import styles from '../style/dashboard.module.css';
 
 
 interface Props {
 
+
 }
 
 interface State {
+  
 
 }
 
 
 class Dashboard extends React.Component<Props, State> {
 
+    static defaultProps = {
+
+    };
+
     constructor(props: Props) {
         super(props);
         this.state = {
 
         };
+
     }
+
 
     componentDidMount(): void {
     }
@@ -30,19 +40,27 @@ class Dashboard extends React.Component<Props, State> {
 
     }
 
-    public render() {
-        return <div></div>
+    createLayout(){
+      const layout = [
+        {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
+        {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
+        {i: 'c', x: 4, y: 0, w: 1, h: 2}
+      ];
+      return layout;
     }
+
+    render() {
+      return (
+        <GridLayout className="layout" layout={this.createLayout()} cols={12} rowHeight={30} width={1200}>
+          <div key="a">a</div>
+          <div key="b">b</div>
+          <div key="c">c</div>
+        </GridLayout>
+      );
+      }
 
 }
 
-const mapStateToProps = (state: model.AppState) => ({
 
-});
-
-const mapDispatchToProps = (dispatch: model.Dispatch) => ({
-
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(withAppContext(Dashboard));
+export default withAppContext(Dashboard);
 

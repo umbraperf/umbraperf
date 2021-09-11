55237d58dcf57f420d76e63e440c7648869e10d0
Johann Schworm <schworm@in.tum.de>
2021-09-11 22:31:50 +0200

    working first add button grid layout

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -16,11 +16,12 @@ interface Props {
 
 interface State {
     items: Array<string>;
-    layout: {lg: Array<ILayoutElement>};
+    layout: { lg: Array<ILayoutElement> };
     gridData: { w: number, h: number, x: number, y: number };
     breakpoints: { lg: number, md: number, sm: number, xs: number, xxs: number };
     cols: { lg: number, md: number, sm: number, xs: number, xxs: number };
     rowHeight: number;
+    newCounter: number;
 
 }
 
@@ -41,12 +42,12 @@ const originalItems: Array<string> = ["a", "b", "c", "d"];
 
 const initialLayouts = {
     lg: [
-      { i: "a", x: 0, y: 0, w: 1, h: 4 },
-      { i: "b", x: 1, y: 0, w: 3, h: 4 },
-      { i: "c", x: 4, y: 0, w: 1, h: 4 },
-      { i: "d", x: 0, y: 4, w: 2, h: 4 }
+        { i: "a", x: 0, y: 0, w: 1, h: 4 },
+        { i: "b", x: 1, y: 0, w: 3, h: 4 },
+        { i: "c", x: 4, y: 0, w: 1, h: 4 },
+        { i: "d", x: 0, y: 4, w: 2, h: 4 }
     ]
-  };
+};
 
 const gridData = { w: 3, h: 2, x: 0, y: Infinity };
 
@@ -68,8 +69,10 @@ class Dashboard extends React.Component<Props, State> {
             breakpoints: { lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 },
             cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
             rowHeight: 60,
-
+            newCounter: 0
         };
+
+        this.onAddItem = this.onAddItem.bind(this);
 
     }
 
@@ -83,29 +86,55 @@ class Dashboard extends React.Component<Props, State> {
 
     render() {
         return (
-            <ResponsiveGridLayout
-            className={`layout ${styles.gridBoxesContainer}`}
-            layouts={this.state.layout}
-                breakpoints={this.state.breakpoints}
-                cols={this.state.cols}
-                rowHeight={this.state.rowHeight}
+            <div>
+                <button onClick={this.onAddItem}>Add Item</button>
+                <ResponsiveGridLayout
+                    className={`layout ${styles.gridBoxesContainer}`}
+                    layouts={this.state.layout}
+                    breakpoints={this.state.breakpoints}
+                    cols={this.state.cols}
+                    rowHeight={this.state.rowHeight}
 /*             onLayoutChange={onLayoutChange}
  */          >
-                {this.state.items.map((key) => (
-                    <div
-                        key={key}
-                        className={`widget ${styles.gridBox}`}
+                    {this.state.items.map((key) => (
+                        <div
+                            key={key}
+                            className={`widget ${styles.gridBox}`}
 /*                         data-grid={this.state.gridData}
  */                    >
-{/*                         <Widget
+                            {/*                         <Widget
                             id={key}
 /*                   onRemoveItem={onRemoveItem}
                    backgroundColor="#867ae9"
                         /> */}
-                    </div>
-                ))}
-            </ResponsiveGridLayout>
+                        </div>
+                    ))}
+                </ResponsiveGridLayout>
+
+            </div>
+
         );
+    }
+
+    onAddItem() {
+        this.setState({
+            // Add a new item. It must have a unique key!
+            items: this.state.items.concat("n" + this.state.newCounter),
+            layout: {
+                lg: this.state.layout.lg.concat({
+                    i: "n" + this.state.newCounter,
+                    x: (this.state.items.length * 2) % (this.state.cols as any || 12),
+                    y: Infinity, // puts it at the bottom
+                    w: 2,
+                    h: 2
+                } as ILayoutElement
+
+                ),
+
+            },
+            // Increment the counter to ensure key is always unique.
+            newCounter: this.state.newCounter + 1
+        });
     }
 
 }

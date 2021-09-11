5e6d97c9efedffcadfccee3467f2f8802d166cc3
Michael Schott <schottmichaelsw@gmail.com>
2021-09-11 16:13:04 +0200

    Same refactoring

---
+++ crate/src/function_api.rs
@@ -0,0 +1,22 @@
+/* 
+    pub fn api() -> RecordBatch {
+
+        for element in restful_str {
+
+            if filter
+
+            
+            if special_operator
+
+
+            if selects
+            
+        }
+        
+    }
+ */
+    // FILTER
+    // /?operator=132
+    // COLUMN
+    // /operator
+    // count?operator

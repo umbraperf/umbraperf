e64d0f720524added2269b27e6d43c6443059a21
Johann Schworm <schworm@in.tum.de>
2021-09-17 15:42:35 +0200

    change structure for util component css, style pipeline selector

--- src/components/utils/tab_panel.tsx
+++ src/components/utils/tab_panel.tsx
@@ -3,9 +3,6 @@ import { makeStyles, Theme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import HelpIcon from '@material-ui/icons/Help';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 
 import { AppState } from '../../model/state';
 import * as model from '../../model';

92b7d8b95def6a17a6d70d8e78cea00dfb79b699
Nolimimer <schottmichaelsw@gmail.com>
2021-09-26 20:30:16 +0200

    Abs frequency for event

--- crate/src/exec/freq/abs_freq.rs
+++ crate/src/exec/freq/abs_freq.rs
@@ -40,6 +40,89 @@ pub fn create_abs_freq_bucket(
     .unwrap()
 }
 
+pub fn abs_freq_of_event(
+    batch: &RecordBatch,
+    column_for_event: usize,
+    column_for_time: usize,
+    bucket_size: f64,
+) -> RecordBatch {
+    let batch = &sort_batch(batch, 2);
+
+    let unique_event = find_unique_string(batch, column_for_event);
+
+    // Vector of unique strings
+    let vec_event = unique_event
+        .column(0)
+        .as_any()
+        .downcast_ref::<StringArray>()
+        .unwrap();
+
+    let mut result_bucket = Vec::new();
+    let mut result_vec_event = Vec::new();
+    let mut result_builder = Vec::new();
+
+    let mut time_bucket = bucket_size;
+    let mut column_index = 0;
+
+    let event_column = batch
+        .column(column_for_event)
+        .as_any()
+        .downcast_ref::<StringArray>()
+        .unwrap();
+    let time_column = batch
+        .column(column_for_time)
+        .as_any()
+        .downcast_ref::<Float64Array>()
+        .unwrap();
+
+    let mut bucket_map = HashMap::new();
+    for event in vec_event {
+        bucket_map.insert(event.unwrap(), 0.0);
+    }
+
+    bucket_map.insert("sum", 0.0);
+
+    for time in time_column {
+        let current_event = event_column.value(column_index as usize);
+        while time_bucket < time.unwrap() {
+            for event in vec_event {
+                if bucket_map.get("sum").unwrap() > &0.0 {
+                    let event = event.unwrap();
+                    result_bucket.push((f64::trunc(time_bucket * 100.0) / 100.0) - bucket_size);
+                    result_vec_event.push(event);
+                    let frequenzy = bucket_map.get("sum").unwrap();
+                    result_builder.push(frequenzy.to_owned());
+                    // reset bucket_map
+                    bucket_map.insert(event, 0.0);
+                }
+            }
+
+            // reset sum
+            bucket_map.insert("sum", 0.0);
+            while time_bucket < time.unwrap() {
+                time_bucket += bucket_size;
+            }
+        }
+
+        //if pipelines.contains(&current_pipeline) || pipelines.len() == 0 {
+        bucket_map.insert(
+            current_event,
+            bucket_map.get(current_event).unwrap() + 1.0,
+        );
+        //}
+        bucket_map.insert("sum", bucket_map.get("sum").unwrap() + 1.0);
+        column_index += 1;
+    }
+
+    create_abs_freq_bucket(
+        &batch,
+        column_for_event,
+        result_bucket,
+        result_vec_event,
+        result_builder,
+    )
+}
+
 pub fn abs_freq_with_pipelines(
     batch: &RecordBatch,
     column_for_operator: usize,

3ee3c6264273f48a672535433d9c49a285e8d13c
Johann Schworm <schworm@in.tum.de>
2021-10-05 13:47:18 +0200

    add util component for kpis

---
+++ src/components/utils/kpi_container.tsx
@@ -0,0 +1,57 @@
+import * as model from '../../model';
+import * as Controller from '../../controller/request_controller';
+import * as Context from '../../app_context';
+import styles from '../../style/utils.module.css';
+import React from 'react';
+import { connect } from 'react-redux';
+
+
+interface Props {
+
+
+}
+
+interface State {
+
+}
+
+class KpiContainer extends React.Component<Props, State> {
+
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+
+        };
+
+    }
+
+    componentDidUpdate(prevProps: Props): void {
+
+
+    }
+
+    componentDidMount() {
+
+    }
+
+
+
+    public render() {
+
+        return <div >
+        </div>;
+    }
+
+
+}
+
+const mapStateToProps = (state: model.AppState) => ({
+
+});
+
+const mapDispatchToProps = (dispatch: model.Dispatch) => ({
+
+});
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Context.withAppContext(KpiContainer));

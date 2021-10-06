799fc5f01c06762fc4869fec84ba8ecf85992c34
Johann Schworm <schworm@in.tum.de>
2021-10-06 22:12:07 +0200

    code cleanup activity histogram

--- src/components/charts/bar_chart_activity_histogram.tsx
+++ src/components/charts/bar_chart_activity_histogram.tsx
@@ -217,7 +217,7 @@ class BarChartActivityHistogram extends React.Component<Props, State> {
                             init: "[calcXScale0,calcXScale1]",
                             on: [
                                 {
-                                    events: [{ type: "mousedown", marktype: "group" }, { type: "mousedown", marktype: "rect" }],//"@overview:mousedown",
+                                    events: [{ type: "mousedown", marktype: "group" }, { type: "mousedown", marktype: "rect" }],
                                     update: "[x(), x()]"
                                 },
                                 {

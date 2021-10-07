84ff4a6d98d81388eb2c41fea02ae01314814135
Johann Schworm <schworm@in.tum.de>
2021-10-07 14:32:30 +0200

    init styled kpi container and kpi cards, automated kpi card creation out of component state array (todo: use array from redux)

--- src/components/utils/kpi_container.tsx
+++ src/components/utils/kpi_container.tsx
@@ -4,34 +4,73 @@ import * as Context from '../../app_context';
 import styles from '../../style/utils.module.css';
 import React from 'react';
 import { connect } from 'react-redux';
+import { Card, CardContent, Typography } from '@material-ui/core';
 
 
 interface Props {
-
+    appContext: Context.IAppContext;
 
 }
 
 interface State {
+    kpiDataArray: Array<IKpiData>;
+}
 
+interface IKpiData {
+    title: string,
+    body: string,
+    explanation: string,
 }
 
 class KpiContainer extends React.Component<Props, State> {
 
     constructor(props: Props) {
         super(props);
+        //add 2 dummy cards: 
         this.state = {
-
+            ...this.state,
+            kpiDataArray: [{ title: "Test 1", body: "KPI 1", explanation: "Explanation of KPI 1." }, { title: "Test 2", body: "KPI 2", explanation: "Explanation of KPI 2." }],
         };
 
+        this.mapKpiArrayToCards = this.mapKpiArrayToCards.bind(this);
     }
 
     componentDidUpdate(prevProps: Props): void {
+
+        //Map redux kpi to state kpi? or use directly from redux...
 
 
     }
 
     componentDidMount() {
+        //add 2 dummy cards: 
+        /*         let dummyKpiData = [{title: "Test 1", body:"KPI 1", explanation:"Explanation of KPI 1."}, {title: "Test 2", body:"KPI 2", explanation:"Explanation of KPI 2."}];
+                this.setState((state, props) => ({
+                    ...state,
+                    kpiDataArray: dummyKpiData,
+                })); */
+    }
 
+    mapKpiArrayToCards() {
+        //get array of kpis from redux, map to multiple cards
+        const kpiCardsArray = this.state.kpiDataArray.map(elem => this.createKpiCard(elem.title, elem.body, elem.explanation));
+        return kpiCardsArray;
+    }
+
+    createKpiCard(title: string, body: string, explanation: string) {
+        return <Card className={styles.kpiCard}>
+            <CardContent>
+                <Typography color="secondary">
+                    {title}
+                </Typography>
+                <Typography variant="h5" component="div">
+                    {body}
+                </Typography>
+                <Typography variant="body2">
+                    {explanation}
+                </Typography>
+            </CardContent>
+        </Card>
     }
 
 
@@ -39,7 +78,7 @@ class KpiContainer extends React.Component<Props, State> {
     public render() {
 
         return <div className={styles.kpiContainer} >
-            Interesting query fun facts will be displayed here!
+            {this.state.kpiDataArray && this.mapKpiArrayToCards()}
         </div>;
     }
 

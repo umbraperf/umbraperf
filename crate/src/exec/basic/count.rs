e5f7d6140360bf91bb21a2490c285e754e77c8aa
Nolimimer <schottmichaelsw@gmail.com>
2021-10-09 08:38:36 +0200

    Relative and max kpis

--- crate/src/exec/basic/count.rs
+++ crate/src/exec/basic/count.rs
@@ -4,6 +4,8 @@ use arrow::{array::{Array, Float64Array, StringArray}, datatypes::{DataType, Fie
 
 use crate::{exec::basic::analyze::{filter_with, find_unique_string}, get_record_batches};
 
+use super::analyze;
+
 pub fn count(batch: &RecordBatch, column_to_count: usize) -> RecordBatch {
 
     let vec = batch
@@ -92,4 +94,71 @@ pub fn count_rows_over(batch: &RecordBatch, column_to_groupby_over: usize) -> Re
 
     let batch = RecordBatch::try_new(Arc::new(schema), vec![vec, Arc::new(builder)]).unwrap();
     return batch;
+}
+
+
+pub fn max_execution_time(batch: &RecordBatch, column_index_for_max: usize) -> RecordBatch {
+
+    let vec = batch
+    .column(column_index_for_max)
+    .as_any()
+    .downcast_ref::<Float64Array>()
+    .unwrap();
+
+    let max = arrow::compute::max(vec).unwrap();
+
+    let mut result_builder = Float64Array::builder(1);
+
+    let _result_builder = result_builder.append_value(max);
+    let result_builder = result_builder.finish();
+
+    let result_field = Field::new("count", DataType::Float64, false);
+
+    let schema = Schema::new(vec![result_field]);
+
+    let batch = RecordBatch::try_new(Arc::new(schema), vec![Arc::new(result_builder)]).unwrap();
+    
+    batch
+
+}
+
+
+pub fn relative(batch: &RecordBatch, column_index_for_numerator: usize, column_index_for_denominator: usize) -> RecordBatch {
+
+    let vec_de = batch
+    .column(column_index_for_denominator)
+    .as_any()
+    .downcast_ref::<StringArray>()
+    .unwrap();
+
+    let denominator = vec_de.len();
+
+    let batch = analyze::filter_with(column_index_for_numerator, vec!["No Operator"], batch);
+
+    let vec_nom = batch
+    .column(column_index_for_numerator)
+    .as_any()
+    .downcast_ref::<StringArray>()
+    .unwrap();
+
+    let numerator = vec_nom.len();
+
+    let result = (numerator / denominator) as f64; 
+
+    let mut result_builder = Float64Array::builder(1);
+
+    let _result_builder = result_builder.append_value(result);
+
+    let result_builder = result_builder.finish();
+
+    let result_field = Field::new("count", DataType::Float64, false);
+
+    let schema = Schema::new(vec![result_field]);
+
+    let batch = RecordBatch::try_new(Arc::new(schema), vec![Arc::new(result_builder)]).unwrap();
+    
+    batch
+
+
+
 }

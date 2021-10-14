9d4d8028f98c9fc7f926e8d71bfe300315be47fa
Johann Schworm <schworm@in.tum.de>
2021-10-14 16:45:31 +0200

    debugging info abs combined rest query

--- src/model/rest_queries.ts
+++ src/model/rest_queries.ts
@@ -82,7 +82,7 @@ export function createRestQuery(query: QueryVariant) {
         case RestQueryType.GET_REL_OP_DISTR_PER_BUCKET_PER_MULTIPLE_PIPELINES_COMBINED_EVENTS:
             return `bucket/operator/relfreq/bucketNEG/operatorNEG/relfreqNEG${timeFilter}/relfreq?pipeline,${bucketSize}!${pipelines}&${query.data.event1},${query.data.event2}`;
         case RestQueryType.GET_ABS_OP_DISTR_PER_BUCKET_PER_MULTIPLE_PIPELINES_COMBINED_EVENTS:
-            console.log(timeFilter);
+            console.log(`bucket/operator/absfreq/bucketNEG/operatorNEG/absfreqNEG${timeFilter}/relfreq?pipeline,${bucketSize}!${pipelines}&${query.data.event1},${query.data.event2}`);
             return `bucket/operator/absfreq/bucketNEG/operatorNEG/absfreqNEG${timeFilter}/relfreq?pipeline,${bucketSize}!${pipelines}&${query.data.event1},${query.data.event2}`;
         case RestQueryType.GET_PIPELINE_COUNT:
             return `pipeline/count${eventFilter}${timeFilter}/count?pipeline/sort?pipeline`;

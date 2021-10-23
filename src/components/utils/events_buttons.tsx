1106368578ffab8c5badc23b2f6224a42559f0ef
Johann Schworm <schworm@in.tum.de>
2021-10-24 00:46:57 +0200

    fix bug in event buttons -> set current event when tuple is selected correctly

--- src/components/utils/events_buttons.tsx
+++ src/components/utils/events_buttons.tsx
@@ -37,7 +37,7 @@ function EventsButtons(props: Props) {
         if (props.multipleEvents) {
             const newMultipleEventsTuple: [string, string] = [props.currentMultipleEvent[1], event];
             props.setCurrentMultipleEvent(newMultipleEventsTuple);
-            props.setCurrentEvent(props.currentMultipleEvent[0]);
+            props.setCurrentEvent(props.currentMultipleEvent[1]);
         } else {
             props.setCurrentEvent(event);
         }

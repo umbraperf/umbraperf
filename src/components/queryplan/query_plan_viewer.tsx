ce0ab86998aa62616aca5102194488e74ca94486
Johann Schworm <schworm@in.tum.de>
2021-11-05 14:53:20 +0100

    add new react component queryPlanViewer with window resize logic

---
+++ src/components/queryplan/query_plan_viewer.tsx
@@ -0,0 +1,123 @@
+import * as model from '../../model';
+import * as Context from '../../app_context';
+import Spinner from '../utils/spinner';
+import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { createRef } from 'react';
+import _ from 'lodash';
+
+interface Props {
+    appContext: Context.IAppContext;
+    csvParsingFinished: boolean;
+    queryPlan: object |undefined;
+    setCurrentChart: (newCurrentChart: string) => void;
+}
+
+interface State {
+    width: number,
+}
+
+
+class QueryPlanViewer extends React.Component<Props, State> {
+
+    elementWrapper = createRef<HTMLDivElement>();
+
+
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            width: 0,
+        };
+        
+    }
+
+    shouldComponentUpdate(nextProps: Props, nextState: State) {
+        return true;
+    }
+
+    componentDidUpdate(prevProps: Props): void {
+    }
+
+
+    componentDidMount() {
+
+        this.setState((state, props) => ({
+            ...state,
+            width: this.elementWrapper.current!.offsetWidth,
+        }));
+
+        if (this.props.csvParsingFinished) {
+            this.props.setCurrentChart(model.ChartType.QUERY_PLAN);
+
+            addEventListener('resize', (event) => {
+                this.resizeListener();
+            });
+        }
+    }
+
+    resizeListener() {
+        if (!this.elementWrapper) return;
+
+        const child = this.elementWrapper.current;
+        if (child) {
+            const newWidth = child.offsetWidth;
+
+            child.style.display = 'none';
+
+            this.setState((state, props) => ({
+                ...state,
+                width: newWidth,
+            }));
+
+            child.style.display = 'block';
+        }
+
+    }
+
+    isComponentLoading(): boolean {
+        if (!this.props.queryPlan) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public render() {
+
+        if (!this.props.csvParsingFinished) {
+            return <Redirect to={"/upload"} />
+        }
+
+        return <div ref={this.elementWrapper} style={{ position: "relative", display: "flex", height: "100%" }}>
+            {this.isComponentLoading()
+                ? <Spinner />
+                : <div className={"queryplanContainer"} >
+                    {this.createDagreQueryPlan()}
+                    <p>queryplan will be here</p>
+                </div>
+            }
+        </div>;
+    }
+
+    createDagreQueryPlan(){
+        const queryPlanJson = this.props.queryPlan;
+        return queryPlanJson;
+    }
+}
+
+
+const mapStateToProps = (state: model.AppState) => ({
+    csvParsingFinished: state.csvParsingFinished,
+    queryPlan: state.queryPlan,
+});
+
+const mapDispatchToProps = (dispatch: model.Dispatch) => ({
+    setCurrentChart: (newCurrentChart: string) => dispatch({
+        type: model.StateMutationType.SET_CURRENTCHART,
+        data: newCurrentChart,
+    }),
+});
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Context.withAppContext(QueryPlanViewer));

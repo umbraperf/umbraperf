c020db300e5692fdfedde04bcc9238d15f5aa4fc
Michael Schott <ga48xop@in.tum.de>
2021-11-15 16:01:58 +0100

    display only when possible

--- crate/src/exec/basic/uir.rs
+++ crate/src/exec/basic/uir.rs
@@ -20,65 +20,88 @@ pub fn uir(file_length: u64, record_batch: RecordBatch) -> RecordBatch {
 
     let dict = get_serde_dict().unwrap();
 
-    let mut mapping_vec = Vec::new();
-    // ev_name, uir, count (+ global sum)
+    let mut srcline_key_vec = Vec::new();
     let mut hashmap_count = HashMap::new();
-    let mut unqiue_events = HashSet::new();
+    let mut unique_events_set = HashSet::new();
     for entry in column.into_iter().enumerate() {
-        let e = entry.1.unwrap();
-        let hashmap = dict.dict.get("srclines").unwrap();
-        let mapping = hashmap.get(&(e as u64)).unwrap();
+        let entry_num = entry.1.unwrap();
+        let hashmap_samplekey_srcline = dict.dict.get("srclines").unwrap();
+        let srcline = hashmap_samplekey_srcline.get(&(entry_num as u64)).unwrap();
         let current_ev = column_ev_name.value(entry.0);
-        if mapping.contains("dump") || mapping.contains("proxy") {
-            let split = mapping.split_terminator(":").collect::<Vec<&str>>();
-            mapping_vec.push(split[1]);
-
-            unqiue_events.insert(current_ev);
+        if srcline.contains("dump") || srcline.contains("proxy") {
+            let split = srcline.split_terminator(":").collect::<Vec<&str>>();
+            let srcline_key = split[1];
             let inner_hashmap = hashmap_count.entry(current_ev).or_insert(HashMap::new());
-            inner_hashmap.entry(split[1]).or_insert(0);
+            srcline_key_vec.push(srcline_key);
+            unique_events_set.insert(current_ev);
+            inner_hashmap.entry(srcline_key).or_insert(0);
+            inner_hashmap.insert(srcline_key, inner_hashmap[split[1]] + 1);
             inner_hashmap.entry("sum").or_insert(0);
-            inner_hashmap.insert(split[1], inner_hashmap[split[1]] + 1);
             inner_hashmap.insert("sum", inner_hashmap["sum"] + 1);
-
         }
     }
 
     print_to_js_with_obj(&format!("hashmap {:?}", hashmap_count).into());
 
-
-
     let mut output_vec = Vec::new();
 
-
     let keys = dict.uri_dict.keys();
-    let mut vec = keys.collect::<Vec<&String>>();
+    let vec = keys.collect::<Vec<&String>>();
     let mut vec = vec.into_iter().map(|i| i.parse::<i64>().unwrap()).collect::<Vec<i64>>();
     vec.sort();
-    print_to_js_with_obj(&format!("vec {:?}", vec).into());
-
 
     for entry in vec {
         let entry = entry.to_string();
-        let mut vec = Vec::new();
-        for event in &unqiue_events {
+        let mut buffer_percentage = Vec::new();
+        for event in &unique_events_set {
             let inner_hashmap = hashmap_count.get(event).unwrap();
             print_to_js_with_obj(&format!("summe {:?}", inner_hashmap.get("sum")).into()); 
             let specific = *inner_hashmap.get(&(entry.as_str())).unwrap_or(&0) as f64;
             print_to_js_with_obj(&format!("specific {:?}", specific).into());
             let total = *inner_hashmap.get("sum").unwrap() as f64;
             let percentage = specific / total;
-            let percentage = f64::trunc((percentage) * 100.0) / 100.0;
-            let out = format!("{}% ", percentage.to_string());
- 
-            vec.push(out);
+            let percentage = f64::trunc((percentage) * 1000.0) / 1000.0;
+            buffer_percentage.push(percentage)
+        }
+
+        let mut vec = Vec::new();
+
+        let mut hasEntries = false;
+        for perc in &buffer_percentage {
+            if perc > &0. {
+                hasEntries = true;
+                break;
+            }
+        }
+        
+        if hasEntries {
+            vec.push("// ".to_string());
+            for perc in buffer_percentage {
+                let out = format!("{}% ", perc.to_string());
+                vec.push(out);
+            }
         }
 
         let d = dict.uri_dict.get(&entry).unwrap();
-        let mut output = format!("{}: {} | \n ", entry, &d.uir.as_ref().unwrap());
+        let srcline =  &d.uir.as_ref().unwrap();
+        let len_srcline = srcline.chars().count();
+        if len_srcline < 140 {
+            let diff = 140-len_srcline;
+            let repeat = " ".repeat(diff);
+            let mut output = format!("{} {}", &d.uir.as_ref().unwrap(), repeat);
+            for e in vec {
+                output.push_str(&e);
+            }
+            output.push_str(" \n");
+            output_vec.push(output);
+        } else {
+        let mut output = format!("{}", &d.uir.as_ref().unwrap());
         for e in vec {
             output.push_str(&e);
         }
+        output.push_str(" \n");
         output_vec.push(output);
+        }
     }
 
     let out_batch = create_new_record_batch(vec!["scrline"], vec![DataType::Utf8], vec![Arc::new(StringArray::from(output_vec))]);

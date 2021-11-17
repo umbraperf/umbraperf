dc45e3fc121958a6e1eb601511d42e16bf4cab9c
Johann Schworm <schworm@in.tum.de>
2021-11-17 19:55:20 +0100

    disable dropdowns on dashobard header logic, diable dropdowns on dashbaord header on uir dashboard, diable interpolation dropdown on memory dashboard

--- src/components/utils/dropdowns_options.tsx
+++ src/components/utils/dropdowns_options.tsx
@@ -1,15 +1,35 @@
+import * as model from '../../model';
 import React from 'react';
 import styles from '../../style/utils.module.css';
 import InterpolationDropdown from '../utils/interpolation_dropdown';
 import BucketsizeDropdwn from '../utils/bucketsize_dropdown';
+import { connect } from 'react-redux';
 
-function DropdownsOptions() {
+interface Props {
+    currentView: model.ViewType;
+}
+
+function DropdownsOptions(props: Props) {
+
+    const isDropdownDisabled = (dropdownType: "interpolationDropdown" | "bucketsizeDropdown") => {
+        if(dropdownType === "interpolationDropdown" && props.currentView === model.ViewType.DASHBOARD_MEMORY){
+            return true;
+        }
+        if (props.currentView === model.ViewType.DASHBOARD_UIR) {
+            return true;
+        }
+        return false;
+    }
 
     return <div className={styles.dropdownOptionsContainer}>
-        <InterpolationDropdown />
-        <BucketsizeDropdwn />
+        <InterpolationDropdown disabled={isDropdownDisabled("interpolationDropdown")} />
+        <BucketsizeDropdwn disabled={isDropdownDisabled("bucketsizeDropdown")} />
     </div>
 
 }
 
-export default DropdownsOptions;
+const mapStateToProps = (state: model.AppState) => ({
+    currentView: state.currentView,
+});
+
+export default connect(mapStateToProps)(DropdownsOptions);

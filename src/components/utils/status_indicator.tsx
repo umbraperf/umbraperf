2a1ccbf9e193041003d9c06657d4e5c77841d212
Johann Schworm <schworm@in.tum.de>
2021-11-17 21:47:33 +0100

    add conditions for status indicator

--- src/components/utils/status_indicator.tsx
+++ src/components/utils/status_indicator.tsx
@@ -9,6 +9,9 @@ interface Props {
     appContext: Context.IAppContext;
     fileLoading: boolean;
     file: undefined | File;
+    resultLoading: model.ResultLoading;
+    chartData: model.ChartDataKeyValue;
+
 }
 
 function StatusIndicator(props: Props) {
@@ -19,13 +22,29 @@ function StatusIndicator(props: Props) {
     }
 
     const getCurrentStatus = () => {
-        if(undefined === props.file && false === props.fileLoading){
+        if (undefined === props.file && false === props.fileLoading) {
             return "No file selected.";
         }
-        if(true === props.fileLoading && props.file){
+        if (true === props.fileLoading && props.file) {
             return `Reading file (${(props.file.name.length > 20) ? props.file.name.substr(0, 20) + '&hellip;' : props.file.name})...`;
         }
-        return "";
+        if(!isResultLoading() && Object.keys(props.chartData).length === 0){
+            return "Initialising...";
+        }
+        if(isResultLoading()){
+            // TODO name rendering chart
+            return `Rendering ${"xxx"}...`
+        }
+        return "Done.";
+    }
+
+    const isResultLoading = () => {
+        for (let resultId in props.resultLoading) {
+            if (true === props.resultLoading[resultId]) {
+                return true;
+            }
+        }
+        return false;
     }
 
     return (
@@ -36,6 +55,8 @@ function StatusIndicator(props: Props) {
 const mapStateToProps = (state: model.AppState) => ({
     file: state.file,
     fileLoading: state.fileLoading,
+    resultLoading: state.resultLoading,
+    chartData: state.chartData,
 });
 
 

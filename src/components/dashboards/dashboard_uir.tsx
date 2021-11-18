4408e08fb64d6c1b7a15b485ca9a557e038e817d
Johann Schworm <schworm@in.tum.de>
2021-11-19 00:34:24 +0100

    fix css bugs single event layout

--- src/components/dashboards/dashboard_uir.tsx
+++ src/components/dashboards/dashboard_uir.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import * as model from '../../model';
 import styles from '../../style/dashboard.module.css';
 import ChartWrapper from '../charts/chart_wrapper';
+import QueryPlanWrapper from '../charts/queryplan/query_plan_wrapper';
 import { Grid, Box } from '@material-ui/core';
 
 
@@ -21,15 +22,23 @@ class DashboardUir extends React.Component<{}, {}> {
             </Box>
 
             <Box clone order={{ xs: 2, md: 2, lg: 2 }}>
-                <Grid item className={styles.dashboardGridCellItemFullScreenStretchContent} xs={12} md={12} lg={12} >
-                    <Box className={styles.dashboardGridCellChartBoxAutoheightFullheightStretchChart}>
-                        <div className={styles.dashboardGridCellChartContainer}>
-                            <ChartWrapper chartType={model.ChartType.UIR_VIEWER} />
-                        </div>
+                <Grid item container direction="row" className={styles.dashboardGridCellItemFullScreenStretchContent} xs={12} md={12} lg={12}>
+                    <Box clone order={{ xs: 1, md: 1, lg: 1 }}>
+                        <Grid item className={styles.dashboardGridCellChartBoxAutoheightFullheightStretchChart} xs={12} md={9} lg={9}>
+                            <div className={styles.dashboardGridCellChartContainer}>
+                                <ChartWrapper chartType={model.ChartType.UIR_VIEWER} />
+                            </div>
+                        </Grid>
+                    </Box>
+                    <Box clone order={{ xs: 2, md: 2, lg: 2 }}>
+                        <Grid item className={styles.dashboardGridCellChartBoxAutoheightFullheightStretchChart} xs={12} md={3} lg={3}>
+                            <div className={styles.dashboardGridCellChartContainer}>
+                                <QueryPlanWrapper />
+                            </div>
+                        </Grid>
                     </Box>
                 </Grid>
             </Box>
-
 
         </Grid>
     }

f8cd6f6e163031d22dfe564a4cb3ad5ef2cf0d14
Johann Schworm <schworm@in.tum.de>
2021-11-19 04:42:52 +0100

    add mini spinner and mini spinner todo to status indicator

---
+++ src/components/utils/mini_spinner.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../../style/mini-spinner.module.css';
+
+function MiniSpinner() {
+
+    return <div className={styles.spinner}/>
+
+}
+
+export default MiniSpinner;

94c5436b26636d6396dd941580aebcf3305fd813
Johann Schworm <schworm@in.tum.de>
2021-11-21 15:28:03 +0100

    clean imports of vega heatmaps

--- src/components/charts/vega_visualizations/memory_access_heatmap_chart.tsx
+++ src/components/charts/vega_visualizations/memory_access_heatmap_chart.tsx
@@ -6,8 +6,6 @@ import { connect } from 'react-redux';
 import { Vega } from 'react-vega';
 import { VisualizationSpec } from "react-vega/src";
 import _ from 'lodash';
-import HeatmapsDiffToggler from '../../utils/togglers/heatmaps_difference_toggler';
-
 
 interface AppstateProps {
     appContext: Context.IAppContext;

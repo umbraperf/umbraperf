5a5ca8922791f13e3224023640421ad609491aa5
Nolimimer <schottmichaelsw@gmail.com>
2021-11-27 10:56:42 +0100

    resolved unused imports and other small changes

--- crate/src/exec/rest/rest_api.rs
+++ crate/src/exec/rest/rest_api.rs
@@ -1,4 +1,4 @@
-use crate::{exec::basic::{basic, count, filter, kpis, uir::{get_top_functions, get_top_srclines, uir}}, record_batch_util::send_record_batch_to_js, state::state::{get_file_size, get_query_from_cache, insert_query_to_cache}, utils::{print_to_cons::print_to_js_with_obj, record_batch_util::concat_record_batches, string_util::{split_at_and, split_at_comma, split_at_double_and, split_at_question_mark, split_at_to}}};
+use crate::{exec::basic::{basic, count, filter, kpis, uir::{get_top_functions, get_top_srclines, uir}}, record_batch_util::send_record_batch_to_js, state::state::{get_file_size, get_query_from_cache, insert_query_to_cache}, utils::{print_to_cons::print_to_js_with_obj, record_batch_util::concat_record_batches, string_util::{split_at_comma, split_at_double_and, split_at_question_mark, split_at_to}}};
 use arrow::record_batch::RecordBatch;
 use std::usize;
 

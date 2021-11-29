e15f476bbb58b73b517cb8430b95215c9a96790e
Johann Schworm <schworm@in.tum.de>
2021-11-29 16:25:01 +0100

    init comment out dropdown prfiles, init place container for dropdown

--- src/components/utils/dropdowns/profiles_dropdown.tsx
+++ src/components/utils/dropdowns/profiles_dropdown.tsx
@@ -2,7 +2,7 @@ import * as model from '../../../model';
 import * as Context from '../../../app_context';
 import React from 'react';
 import MenuItem from '@material-ui/core/MenuItem';
-import { FormControl, InputLabel, Select } from '@material-ui/core';
+import { FormControl, InputLabel, makeStyles, Select } from '@material-ui/core';
 import styles from "../../../style/utils.module.css";
 import { connect } from 'react-redux';
 
@@ -15,6 +15,37 @@ interface AppstateProps {
 
 type Props = AppstateProps;
 
+const useMenuPropsStyles = makeStyles({
+    select: {
+        "& ul": {
+            backgroundColor: "#cccccc",
+        },
+        "& li": {
+            fontSize: 1,
+        },
+    }
+});
+
+const useStyles = makeStyles({
+    select: {
+        '&:before': {
+            borderColor: 'white',
+        },
+        '&:after': {
+            borderColor: 'white',
+        },
+        '&:not(.Mui-disabled):hover::before': {
+            borderColor: 'white',
+        },
+    },
+    icon: {
+        fill: 'white',
+    },
+    root: {
+        color: 'white',
+    },
+});
+
 function ProfilesDropdown(props: Props) {
 
     const profiles = [
@@ -26,23 +57,50 @@ function ProfilesDropdown(props: Props) {
     };
 
 
+    const classes = useStyles();
+    const menuClasses = useMenuPropsStyles();
+
     return (
+
         <div className={styles.profilesDropdownSelectorContainer}>
-            <FormControl fullWidth>
-                <InputLabel className={styles.profilesDropdownSelectorLabel} style={{ color: props.appContext.tertiaryColor }} id="interpolation-selector-label">Interpolation: </InputLabel>
-                <Select className={styles.profilesDropdownSelector}
+
+            test
+            {/* <div className={styles.profilesDropdownSelectorContainer}>
+            <FormControl
+                className={styles.profilesDropdownSelectorControl}
+                fullWidth
+                size='small'
+                variant='outlined'>
+                <InputLabel id="profiles-selector-label" className={styles.profilesDropdownSelectorLabel} style={{ color: props.appContext.tertiaryColor }} >Interpolation: </InputLabel>
+                <Select
+                    inputProps={{
+                        classes: {
+                            icon: classes.icon,
+                            root: classes.root,
+                        },
+                    }}
+                    MenuProps={{
+                        classes: {
+                            paper: menuClasses.select
+                        }
+                    }}
+                    className={classes.select}
+                    // className={styles.profilesDropdownSelector}
                     color="secondary"
-                    labelId="interpolation-selector-label"
-                    id="interpolation-selector"
-                    value={profiles[1]}
-                    disabled={false /* disable while csv loading */}
+                    labelId="profiles-selector-label"
+                    id="profiles-selector"
+/*                     value={profiles[1]}
+                    disabled={false /* disable while csv loading }
                 >
                     {profiles.map((elem, index) =>
                         (<MenuItem onClick={() => handleOnItemClick(elem)} key={index} value={elem}>{elem}</MenuItem>)
                     )}
                 </Select>
             </FormControl >
+        </div> */
+            }
         </div>
+
     );
 }
 
--- src/components/utils/navigation/header_appbar.tsx
+++ src/components/utils/navigation/header_appbar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from "../../../style/utils.module.css";
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import StatusIndicator from './status_indicator';
+import ProfilesDropdown from './../dropdowns/profiles_dropdown';
 
 function HeaderAppbar() {
 
@@ -17,7 +18,8 @@ function HeaderAppbar() {
                     Umbra-Profiler
                 </Typography>
 
-                <div className={styles.appbarToolbarStatusContainer}>
+                <div className={styles.appbarToolbarOptionsContainer}>
+                    <ProfilesDropdown />
                     <StatusIndicator />
                 </div>
             </Toolbar>

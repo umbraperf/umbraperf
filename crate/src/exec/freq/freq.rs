c1085e9cc2da3f413b3364786e8bd265817a0fbe
Michael Schott <schottmichaelsw@gmail.com>
2021-12-01 19:43:34 +0100

    Change heatmap diff calculations

--- crate/src/exec/freq/freq.rs
+++ crate/src/exec/freq/freq.rs
@@ -9,12 +9,21 @@ use arrow::{
     record_batch::RecordBatch,
 };
 
-use crate::{exec::{basic::{basic::{find_unique_string, sort_batch}, filter::{filter_between_int32, filter_between_u64}, statistics}, rest::rest_api::finish_query_exec}, state::state::get_record_batches, utils::{
-        print_to_cons::print_to_js_with_obj,
-        record_batch_util::{
-            create_new_record_batch, send_record_batch_to_js,
+use crate::{
+    exec::{
+        basic::{
+            basic::{find_unique_string, sort_batch},
+            filter::{filter_between_int32, filter_between_u64},
+            statistics,
         },
-    }};
+        rest::rest_api::finish_query_exec,
+    },
+    state::state::get_record_batches,
+    utils::{
+        print_to_cons::print_to_js_with_obj,
+        record_batch_util::{create_new_record_batch, send_record_batch_to_js},
+    },
+};
 
 pub enum Freq {
     ABS,
@@ -262,7 +271,28 @@ pub fn freq_of_pipelines(
 
 pub enum MEM {
     DIFF,
-    ABS
+    ABS,
+}
+
+pub fn get_earlier_entry(
+    recordbatch: &RecordBatch,
+    index: usize,
+    operator: &str,
+    column_for_operator: usize,
+) -> usize {
+    if index == 0 {
+        return usize::MAX;
+    }
+    let mut start = index;
+    while start >= 1 {
+        //print_to_js_with_obj(&format!("curr_operator {:?} compare_with  {:?}", operator, get_stringarray_column(recordbatch, column_for_operator).value(start - 1)).into());
+
+        if operator == get_stringarray_column(recordbatch, column_for_operator).value(start - 1) {
+            return start;
+        }
+        start -= 1;
+    }
+    return usize::MAX;
 }
 
 pub fn freq_of_memory(
@@ -275,24 +305,6 @@ pub fn freq_of_memory(
     len_of_mem: Option<i64>,
     mem_en: MEM,
 ) {
-    /* let batch = if matches!(mem_en, MEM::ABS) {
-        let mem_column = get_uint_column(&batch, 4);
-        let mem_vec = mem_column
-            .into_iter()
-            .map(|v| (v.unwrap() as i64))
-            .collect::<Vec<i64>>();
-
-        let mean = statistics::mean(&mem_vec).unwrap();
-        let std_deviation = statistics::std_deviation(&mem_vec).unwrap();
-        let three_times = std_deviation * std_deviation * std_deviation;
-
-        let from_ = mean - std_deviation;
-        let to_ = mean + std_deviation;
-        let batch = filter_between_u64(4, from_ as f64, to_ as f64, &batch);
-        batch
-    } else {
-        batch.to_owned()
-    }; */
 
     let batch = &sort_batch(&batch, 2, false);
 
@@ -319,7 +331,6 @@ pub fn freq_of_memory(
     }
 
     time_bucket = f64::trunc(time_bucket);
-    let mut column_index = 0;
 
     let mut bucket_map = HashMap::new();
     for operator in vec_operator {
@@ -341,20 +352,28 @@ pub fn freq_of_memory(
         10000000000
     };
 
-    for (i, time) in time_column.into_iter().enumerate() {
-        let current_operator = operator_column.value(column_index as usize);
-        let current_memory = if matches!(mem_en, MEM::ABS) { (memory_column.value(column_index as usize) / divided) as i32 } else {
-            if i == 0 {
-                0 as i32
-            } else {
-                let value1 = memory_column.value(column_index as usize) / 100000;
-                let value2 = memory_column.value(column_index - 1 as usize) / 100000;
-                let diff = value2 as i64 - value1 as i64 ;
-                diff as i32
-            }
+    'outer: for (i, time) in time_column.into_iter().enumerate() {
+        let current_operator = operator_column.value(i as usize);
+        if i == 0 && matches!(mem_en, MEM::ABS) {
+            continue 'outer;
+        } 
+        let value_earlier_index = get_earlier_entry(&batch, i, current_operator, column_for_operator);
+        if (value_earlier_index as usize) == usize::MAX && matches!(mem_en, MEM::DIFF) {
+            continue 'outer;
+        }
+        let current_memory = if matches!(mem_en, MEM::ABS) {
+            (memory_column.value(i as usize) / divided) as i32
+        } else {
+            let value1 = memory_column.value(i as usize);
+
+            let value_earlier = memory_column.value(value_earlier_index as usize);
+
+            let diff = value_earlier as i64 - value1 as i64;
+
+            diff as i32
+
         };
         while time_bucket < time.unwrap() {
-
             for operator in vec_operator {
                 let operator = operator.unwrap();
 
@@ -404,7 +423,6 @@ pub fn freq_of_memory(
             }
         }
 
-        column_index += 1;
     }
 
     let batch = create_mem_bucket(
@@ -415,9 +433,6 @@ pub fn freq_of_memory(
         result_mem_operator,
         result_builder,
     );
-
-    print_to_js_with_obj(&format!("batch {:?}", batch).into());
-
 
     let max_mem = arrow::compute::max(get_int32_column(&batch, 2)).unwrap();
     let min_mem = arrow::compute::min(get_int32_column(&batch, 2)).unwrap();
@@ -451,16 +466,36 @@ pub fn freq_of_memory(
         ],
     );
 
-    send_record_batch_to_js(&meta_info);
+    send_record_batch_to_js(&meta_info); 
 
     let sorted_batch = sort_batch(&batch, 1, false);
     let mut offset = 0.;
     let mut hashmap = HashMap::new();
 
-    print_to_js_with_obj(&format!("entry {:?}", "hi").into());
 
     for entry in vec_operator.into_iter().enumerate() {
+
         let len = bucket_map_count.get(entry.1.unwrap()).unwrap().to_owned();
+
+        if len == 0. {
+            let single_batch = create_new_record_batch(
+                vec!["bucket", "operator", "mem", "freq"],
+                vec![
+                    DataType::Float64,
+                    DataType::Utf8,
+                    DataType::Int32,
+                    DataType::Float64,
+                ],
+                vec![
+                    Arc::new(Float64Array::from(vec![0.])),
+                    Arc::new(StringArray::from(vec![entry.1.unwrap()])),
+                    Arc::new(Int32Array::from(vec![0])),
+                    Arc::new(Float64Array::from(vec![0.])),
+                ],
+            );
+            hashmap.insert((entry.1.unwrap(), 0 as usize), single_batch);
+            continue;
+        } 
 
         let batch = sorted_batch.slice(offset as usize, len as usize);
 
@@ -499,24 +534,32 @@ pub fn freq_of_memory(
                 Arc::new(Int32Array::from(mem_vec)),
                 Arc::new(Float64Array::from(freq_vec)),
             ],
-               
         );
 
         let mem_column = get_int32_column(&single_batch, 2);
-        let mem_vec = mem_column.into_iter().map(|v| (v.unwrap() as i64)).collect::<Vec<i64>>();
-  
+        let mem_vec = mem_column
+            .into_iter()
+            .map(|v| (v.unwrap() as i64))
+            .collect::<Vec<i64>>();
+
         let mean = statistics::mean(&mem_vec).unwrap();
         let std_deviation = statistics::std_deviation(&mem_vec).unwrap();
 
-        let from = if matches!(mem_en, MEM::DIFF) { mean - (std_deviation / 2.) } else {mean - (std_deviation )};
-        let to = if matches!(mem_en, MEM::DIFF) { mean + (std_deviation / 2.) } else {mean + (std_deviation )};
+        let from = if matches!(mem_en, MEM::DIFF) {
+            mean - (std_deviation * 100.)
+        } else {
+            mean - (std_deviation)
+        };
+        let to = if matches!(mem_en, MEM::DIFF) {
+            mean + (std_deviation * 100.)
+        } else {
+            mean + (std_deviation)
+        };
 
         let single_batch = filter_between_int32(2, from as i32, to as i32, &single_batch);
 
-
-        print_to_js_with_obj(&format!("single batch {:?}", single_batch).into());
         let min_bucket = arrow::compute::min(bucket).unwrap();
-        hashmap.insert((entry.1.unwrap(), min_bucket as usize),single_batch);
+        hashmap.insert((entry.1.unwrap(), min_bucket as usize), single_batch);
 
         offset += len;
     }
@@ -533,5 +576,4 @@ pub fn freq_of_memory(
         let batch = hashmap.get(entry).unwrap();
         send_record_batch_to_js(batch);
     }
-
 }

68c489f35ca7fa9492149f2db2af7fed157c024a
Johann Schworm <schworm@in.tum.de>
2021-12-01 10:36:22 +0100

    init switch case in profile controller to handle different profile types

--- src/controller/profile_change_controller.ts
+++ src/controller/profile_change_controller.ts
@@ -1,5 +1,24 @@
 import * as model from '../model';
 
-export function changeProfile(newProfile: model.ProfileType){
-    
-} 
+export function changeProfile(newProfile: model.ProfileType) {
+
+    switch (newProfile) {
+
+        case model.ProfileType.OVERVIEW:
+            break;
+
+        case model.ProfileType.DETAIL_ANALYSIS:
+            break;
+
+        case model.ProfileType.MEMORY_BEHAVIOUR:
+            break;
+
+        case model.ProfileType.CACHE_ANALYSIS:
+            break;
+
+        case model.ProfileType.UIR_ANALYSIS;
+            break;
+
+    }
+
+}

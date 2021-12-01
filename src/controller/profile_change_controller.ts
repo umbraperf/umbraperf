3059f418d8516fd83f967befc7f96ec529ebf556
Johann Schworm <schworm@in.tum.de>
2021-12-01 10:53:15 +0100

    improve profiler menu code

--- src/controller/profile_change_controller.ts
+++ src/controller/profile_change_controller.ts
@@ -5,18 +5,27 @@ export function changeProfile(newProfile: model.ProfileType) {
     switch (newProfile) {
 
         case model.ProfileType.OVERVIEW:
+            console.log("new profile: overview")
             break;
 
         case model.ProfileType.DETAIL_ANALYSIS:
+            console.log("new profile: detail")
+
             break;
 
         case model.ProfileType.MEMORY_BEHAVIOUR:
+            console.log("new profile: memory")
+
             break;
 
         case model.ProfileType.CACHE_ANALYSIS:
+            console.log("new profile: cache")
+
             break;
 
         case model.ProfileType.UIR_ANALYSIS:
+            console.log("new profile: uir")
+
             break;
 
     }

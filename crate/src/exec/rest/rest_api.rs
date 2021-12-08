aa327a8811f1861be2e54e63c261d9a8a9f98a38
Michael Schott <schottmichaelsw@gmail.com>
2021-12-08 19:07:09 +0100

    Init mult events

--- crate/src/exec/rest/rest_api.rs
+++ crate/src/exec/rest/rest_api.rs
@@ -13,7 +13,6 @@ use crate::{
 };
 use arrow::record_batch::RecordBatch;
 use std::usize;
-
 use super::rest_api_pars::{abs_freq_pars, freq_mem, rel_freq_pars, sort};
 
 // Find name in Record Batch
@@ -207,7 +206,6 @@ pub fn finish_query_exec(record_batch: RecordBatch, restful_string: &str) {
 }
 
 pub fn eval_query(record_batch: RecordBatch, restful_string: &str) {
-    print_to_js_with_obj(&format!("{:?}", restful_string).into());
 
     if query_already_calculated(restful_string) {
         return;

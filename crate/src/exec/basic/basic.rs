adffbd3f0d2911c7b4005b7ae0d461001fa1cce8
Michael Schott <ga48xop@in.tum.de>
2021-12-12 22:28:59 +0100

    Simply basic.rs

--- crate/src/exec/basic/basic.rs
+++ crate/src/exec/basic/basic.rs
@@ -10,6 +10,8 @@ use std::{collections::HashSet, sync::Arc};
 use crate::utils::record_batch_util::create_new_record_batch;
 use crate::utils::record_batch_util::create_record_batch;
 
+
+// Specify Vec to "SELECT" those columns
 pub fn select_columns(batch: RecordBatch, columns_to_select: Vec<usize>) -> RecordBatch {
     let mut vec = Vec::new();
     for index in &columns_to_select {
@@ -26,9 +28,10 @@ pub fn select_columns(batch: RecordBatch, columns_to_select: Vec<usize>) -> Reco
     create_record_batch(Arc::new(new_schema), vec)
 }
 
+// "SORT BY" for record batch with descending option
 pub fn sort_batch(
     batch: &RecordBatch,
-    column_index_to_sort: usize,
+    column_for_sort: usize,
     descending: bool,
 ) -> RecordBatch {
     let options = arrow::compute::SortOptions {
@@ -36,42 +39,43 @@ pub fn sort_batch(
         nulls_first: false,
     };
 
-    let indices = sort_to_indices(batch.column(column_index_to_sort), Some(options), None).unwrap();
+    let sorted_array_of_indices = sort_to_indices(batch.column(column_for_sort), Some(options), None).unwrap();
 
     RecordBatch::try_new(
         batch.schema(),
         batch
             .columns()
             .iter()
-            .map(|column| take(column.as_ref(), &indices, None))
+            .map(|column| take(column.as_ref(), &sorted_array_of_indices, None))
             .collect::<ArrowResult<Vec<ArrayRef>>>()
             .unwrap(),
     )
     .unwrap()
 }
 
-pub fn find_unique_string(batch: &RecordBatch, column_index_for_unqiue: usize) -> RecordBatch {
-    let vec = batch
-        .column(column_index_for_unqiue)
+// UNIQUE for specified column
+pub fn find_unique_string(batch: &RecordBatch, column_for_unique: usize) -> RecordBatch {
+    let column = batch
+        .column(column_for_unique)
         .as_any()
         .downcast_ref::<StringArray>()
         .unwrap();
 
-    let hash_set = vec
+    let hash_set = column
         .into_iter()
         .map(|item| item.unwrap())
         .collect::<HashSet<&str>>()
         .into_iter()
         .collect::<Vec<&str>>();
 
-    let array = StringArray::from(hash_set);
+    let string_arr = StringArray::from(hash_set);
 
     let schema = batch.schema();
-    let field = schema.field(column_index_for_unqiue);
+    let field = schema.field(column_for_unique);
     let batch = create_new_record_batch(
         vec![field.name()],
         vec![field.data_type().to_owned()],
-        vec![Arc::new(array)],
+        vec![Arc::new(string_arr)],
     );
 
     return batch;

81eca9ea20cdd90c620944795487fde5cdc032b2
Michael Schott <schottmichaelsw@gmail.com>
2021-12-14 22:54:34 +0100

    Fix bug in double event swimlane

--- crate/src/exec/freq/abs_freq.rs
+++ crate/src/exec/freq/abs_freq.rs
@@ -11,7 +11,11 @@ use crate::{
         basic::{find_unique_string, sort_batch},
         filter,
     },
-    utils::{record_batch_util::create_new_record_batch, record_batch_schema::RecordBatchSchema, array_util::{get_floatarray_column, get_stringarray_column}},
+    utils::{
+        array_util::{get_floatarray_column, get_stringarray_column},
+        record_batch_schema::RecordBatchSchema,
+        record_batch_util::create_new_record_batch,
+    },
 };
 
 use super::freq;
@@ -139,11 +143,19 @@ pub fn abs_freq_with_pipelines_with_double_events(
     from: f64,
     to: f64,
 ) -> RecordBatch {
-    let mut vec = Vec::new();
-    vec.push(events[0]);
-    let f_batch = filter::filter_with(RecordBatchSchema::EvName as usize, vec, batch);
 
-    let (mut bucket_vec, mut op_vec, mut freq_vec, mut bucket_vec_2, mut op_vec_2, mut freq_vec_2) = (
+    let (
+        mut bucket_vec,
+        mut op_vec,
+        mut nice_op_vec,
+        mut freq_vec,
+        mut bucket_vec_2,
+        mut op_vec_2,
+        mut nice_op_vec_2,
+        mut freq_vec_2,
+    ) = (
+        Vec::new(),
+        Vec::new(),
         Vec::new(),
         Vec::new(),
         Vec::new(),
@@ -151,6 +163,8 @@ pub fn abs_freq_with_pipelines_with_double_events(
         Vec::new(),
         Vec::new(),
     );
+
+    let f_batch = filter::filter_with(RecordBatchSchema::EvName as usize, vec![events[0]], batch);
 
     let batch_abs = abs_freq_of_pipelines(
         &f_batch,
@@ -165,24 +179,23 @@ pub fn abs_freq_with_pipelines_with_double_events(
 
     let bucket_col = get_floatarray_column(&batch_abs, 0);
     let op_col = get_stringarray_column(&batch_abs, 2);
-    let freq_col = get_floatarray_column(&batch_abs,3);
+    let nice_op_col = get_stringarray_column(&batch_abs, 1);
+    let freq_col = get_floatarray_column(&batch_abs, 3);
 
     let mut i = 0;
     while i < bucket_col.len() {
         bucket_vec.push(bucket_col.value(i));
         op_vec.push(op_col.value(i));
+        nice_op_vec.push(nice_op_col.value(i));
         freq_vec.push(freq_col.value(i));
         bucket_vec_2.push(-1.0);
         op_vec_2.push("");
+        nice_op_vec_2.push("");
         freq_vec_2.push(0.0);
         i = i + 1;
     }
 
-    let mut vec = Vec::new();
-
-    vec.push(events[1]);
-
-    let batch = filter::filter_with(1, vec, &batch);
+    let batch = filter::filter_with(1, vec![events[1]], &batch);
     let batch_rel = abs_freq_of_pipelines(
         &batch,
         column_for_operator,
@@ -196,15 +209,18 @@ pub fn abs_freq_with_pipelines_with_double_events(
 
     let bucket_col_2 = get_floatarray_column(&batch_rel, 0);
     let op_col_2 = get_stringarray_column(&batch_rel, 2);
-    let freq_col_2 = get_floatarray_column(&batch_rel,3);
+    let nice_op_col_2 = get_stringarray_column(&batch_rel, 1);
+    let freq_col_2 = get_floatarray_column(&batch_rel, 3);
 
     let mut i = 0;
     while i < bucket_col_2.len() {
         bucket_vec.push(-1.0);
         op_vec.push("");
+        nice_op_vec.push("");
         freq_vec.push(0.0);
         bucket_vec_2.push(bucket_col_2.value(i));
         op_vec_2.push(op_col_2.value(i));
+        nice_op_vec_2.push(nice_op_col_2.value(i));
         freq_vec_2.push(freq_col_2.value(i));
         i = i + 1;
     }
@@ -212,25 +228,31 @@ pub fn abs_freq_with_pipelines_with_double_events(
     create_new_record_batch(
         vec![
             "bucketNEG",
+            "op_extNEG",
             "operatorNEG",
             "absfreqNEG",
             "bucket",
+            "op_ext",
             "operator",
             "absfreq",
         ],
         vec![
             DataType::Float64,
             DataType::Utf8,
+            DataType::Utf8,
             DataType::Float64,
             DataType::Float64,
+            DataType::Utf8,
             DataType::Utf8,
             DataType::Float64,
         ],
         vec![
             Arc::new(Float64Array::from(bucket_vec)),
+            Arc::new(StringArray::from(nice_op_vec)),
             Arc::new(StringArray::from(op_vec)),
             Arc::new(Float64Array::from(freq_vec)),
             Arc::new(Float64Array::from(bucket_vec_2)),
+            Arc::new(StringArray::from(nice_op_vec_2)),
             Arc::new(StringArray::from(op_vec_2)),
             Arc::new(Float64Array::from(freq_vec_2)),
         ],

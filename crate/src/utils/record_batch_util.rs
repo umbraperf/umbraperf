12ac9f5724e2bb29d682163305a80681dd37d240
Michael Schott <schottmichaelsw@gmail.com>
2021-12-21 18:15:45 +0100

    Readability improvement

--- crate/src/utils/record_batch_util.rs
+++ crate/src/utils/record_batch_util.rs
@@ -14,6 +14,8 @@ use parquet::{
 };
 use std::{io::Cursor, sync::Arc};
 
+use super::array_util::{get_floatarray_column, get_int64_column, get_uint_column};
+
 pub fn create_record_batch(schema: SchemaRef, columns: Vec<ArrayRef>) -> RecordBatch {
     return RecordBatch::try_new(schema, columns).unwrap();
 }
@@ -127,12 +129,7 @@ pub fn convert_without_mapping(batches: Vec<RecordBatch>) -> RecordBatch {
 pub fn apply_mapping_to_record_batch(batch: RecordBatch) -> RecordBatch {
     let serde = get_serde_dict().unwrap();
 
-    let operator_col = batch
-        .column(0)
-        .as_any()
-        .downcast_ref::<Int64Array>()
-        .unwrap();
-
+    let operator_col = get_int64_column(&batch, 0);
     let mut operator_vec = Vec::new();
     let hash_map = serde.dict.get(&(DictFields::Operator as i64)).unwrap();
     for value in operator_col {
@@ -142,11 +139,7 @@ pub fn apply_mapping_to_record_batch(batch: RecordBatch) -> RecordBatch {
         operator_vec.push(dict_key.unwrap().as_str());
     }
 
-    let event_nam = batch
-        .column(3)
-        .as_any()
-        .downcast_ref::<Int64Array>()
-        .unwrap();
+    let event_nam = get_int64_column(&batch, 3);
 
     let mut event_vec = Vec::new();
     let hash_map = serde.dict.get(&(DictFields::Event as i64)).unwrap();
@@ -155,22 +148,14 @@ pub fn apply_mapping_to_record_batch(batch: RecordBatch) -> RecordBatch {
         event_vec.push(dict_key.unwrap().as_str());
     }
 
-    let time_col = batch
-        .column(2)
-        .as_any()
-        .downcast_ref::<Float64Array>()
-        .unwrap();
+    let time_col = get_floatarray_column(&batch, 2);
 
     let mut time = Vec::new();
     for value in time_col {
         time.push(value.unwrap());
     }
 
-    let pipeline = batch
-        .column(1)
-        .as_any()
-        .downcast_ref::<Int64Array>()
-        .unwrap();
+    let pipeline = get_int64_column(&batch, 1);
 
     let mut pipeline_vec = Vec::new();
     let hash_map = serde.dict.get(&(DictFields::Pipeline as i64)).unwrap();
@@ -179,33 +164,21 @@ pub fn apply_mapping_to_record_batch(batch: RecordBatch) -> RecordBatch {
         pipeline_vec.push(dict_key.unwrap().as_str());
     }
 
-    let addr_col = batch
-        .column(5)
-        .as_any()
-        .downcast_ref::<UInt64Array>()
-        .unwrap();
+    let addr_col = get_uint_column(&batch, 5);
 
     let mut addr = Vec::new();
     for value in addr_col {
         addr.push(value.unwrap());
     }
 
-    let uri_col = batch
-        .column(4)
-        .as_any()
-        .downcast_ref::<Int64Array>()
-        .unwrap();
+    let uri_col = get_int64_column(&batch, 4);
 
     let mut uri = Vec::new();
     for value in uri_col {
         uri.push(value.unwrap());
     }
 
-    let op_ext_col = batch
-        .column(6)
-        .as_any()
-        .downcast_ref::<Int64Array>()
-        .unwrap();
+    let op_ext_col = get_int64_column(&batch, 6);
 
     let mut op_extension = Vec::new();
     let hash_map = serde.dict.get(&(DictFields::OpExtension as i64)).unwrap();
@@ -214,11 +187,7 @@ pub fn apply_mapping_to_record_batch(batch: RecordBatch) -> RecordBatch {
         op_extension.push(dict_key.unwrap().as_str());
     }
 
-    let pyhs_op_col = batch
-        .column(7)
-        .as_any()
-        .downcast_ref::<Int64Array>()
-        .unwrap();
+    let pyhs_op_col = get_int64_column(&batch, 7);
 
     let mut physical_op = Vec::new();
     let hash_map = serde.dict.get(&(DictFields::PhysicalOp as i64)).unwrap();
@@ -226,13 +195,6 @@ pub fn apply_mapping_to_record_batch(batch: RecordBatch) -> RecordBatch {
         let dict_key = hash_map.get(&(value.unwrap() as u64));
         physical_op.push(dict_key.unwrap().as_str());
     }
-
-    /* operator_vec.push("test_Operator");
-    event_vec.push("test_Event");
-    time.push(99.0);
-    pipeline_vec.push("test_Pipeline");
-    addr.push(12341234);
-    uri.push(2);  */
 
     create_new_record_batch(
         vec![

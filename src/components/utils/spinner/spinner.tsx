b215ee83e978e125652adaaa28294b8d842f5686
Johann Schworm <schworm@in.tum.de>
2021-12-23 23:28:11 +0100

    transition for spinner

--- src/components/utils/spinner/spinner.tsx
+++ src/components/utils/spinner/spinner.tsx
@@ -1,16 +1,18 @@
+import { Zoom } from '@material-ui/core';
 import React from 'react';
 import styles from '../../../style/spinner.module.css';
 
 function Spinner() {
 
-    return <div className={styles.spinnerContainer}>
-        <div className={styles.cssloadPiano}>
-            <div className={styles.cssloadRect1}></div>
-            <div className={styles.cssloadRect2}></div>
-            <div className={styles.cssloadRect3}></div>
+    return <Zoom in={true}>
+        <div className={styles.spinnerContainer}>
+            <div className={styles.cssloadPiano}>
+                <div className={styles.cssloadRect1}></div>
+                <div className={styles.cssloadRect2}></div>
+                <div className={styles.cssloadRect3}></div>
+            </div>
         </div>
-    </div>
-
+    </Zoom>
 }
 
 export default Spinner;

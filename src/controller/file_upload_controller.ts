8a5eeeda75c22cd4a14417ac8f289b33c3ccdf06
Johann Schworm <schworm@in.tum.de>
2022-03-18 02:34:28 +0100

    impr

--- src/controller/file_upload_controller.ts
+++ src/controller/file_upload_controller.ts
@@ -8,30 +8,31 @@ export function handleNewFile(file: File){
     appContext.controller.registerFileAtWorker(file);
 }
 
-export function setFile(newFile: File) {
-    store.dispatch({
-        type: model.StateMutationType.SET_FILE,
-        data: newFile,
-    });
-}
-
-export function setUmbraperfFileParsingFinished(newUmbraperfFileParsingFinished: boolean) {
-    store.dispatch({
-        type: model.StateMutationType.SET_UMBRAPERF_FILE_PARSING_FINISHED,
-        data: newUmbraperfFileParsingFinished,
-    });
-}
-
-export function setFileLoading(newFileLoading: boolean) {
-    store.dispatch({
-        type: model.StateMutationType.SET_FILE_LOADING,
-        data: newFileLoading,
-    });
-}
-
 export function resetState() {
     store.dispatch({
         type: model.StateMutationType.SET_RESET_STATE,
         data: undefined,
     });
 }
+
+function setFile(newFile: File) {
+    store.dispatch({
+        type: model.StateMutationType.SET_FILE,
+        data: newFile,
+    });
+}
+
+function setUmbraperfFileParsingFinished(newUmbraperfFileParsingFinished: boolean) {
+    store.dispatch({
+        type: model.StateMutationType.SET_UMBRAPERF_FILE_PARSING_FINISHED,
+        data: newUmbraperfFileParsingFinished,
+    });
+}
+
+function setFileLoading(newFileLoading: boolean) {
+    store.dispatch({
+        type: model.StateMutationType.SET_FILE_LOADING,
+        data: newFileLoading,
+    });
+}
+

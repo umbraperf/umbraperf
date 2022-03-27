3dee14efae232d7fc33c5a113fefa5a2d506ffe8
Johann Schworm <schworm@in.tum.de>
2022-03-27 21:50:40 +0200

    comment out test heatmnaps single event

--- src/components/dashboards/dashboard_memory_accesses.tsx
+++ src/components/dashboards/dashboard_memory_accesses.tsx
@@ -52,14 +52,14 @@ class DashboardMemoryAccesses extends React.Component<Props, {}> {
                 <Grid item className={styles.dashboardGridCellItem} xs={12} md={12} lg={12} >
                     <Box className={styles.dashboardGridCellChartBoxAutoheightChart}>
                         <div className={styles.dashboardGridCellChartContainer}>
-                            {
+                            {/* {
                                 this.memoryInformationAvailable() ?
                                     <p>
                                         No memory information in profiling data provided!
                                     </p>
-                                    :
-                                    <ChartWrapper chartType={model.ChartType.MEMORY_ACCESS_HEATMAP_CHART} />
-                            }
+                                    : */}
+                            <ChartWrapper chartType={model.ChartType.MEMORY_ACCESS_HEATMAP_CHART} />
+                            // }
                         </div>
                     </Box>
                 </Grid>

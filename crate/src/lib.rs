8a6a6768661a48470557898b8dc77153d6d0fbff
Michael S. <schottmichaelsw@gmail.com>
2025-03-25 23:36:54 +0000

    replace all "

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -202,6 +202,8 @@ fn parse_time_filter(filter: &str) -> (Option<f64>, Option<f64>) {
     print_to_js_with_obj(&format!("Parsing time filter: '{}'", filter).into());
 
     let filter = filter.strip_prefix("time=").unwrap_or(filter);
+    // replace all "
+    let filter = filter.replace("\"", "");
 
     if filter.is_empty() || filter == "-1from_to-1" {
         print_to_js_with_obj(&"Empty filter or default value, returning None".into());
